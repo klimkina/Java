e96cd9f0c4f310026967b74ce64b5c12a3988865
Mila Klimkina <klimkina@gmail.com>
2017-11-14 17:25:49 -0800

    week of code 2

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -5,48 +5,49 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-	private static int count47(int n) {
-		int res = 0;
-		char[] charr = Integer.toString(n).toCharArray();
-		for(int i = 0; i < charr.length; i++) {
-			if(charr[i] == '4')
-				res++;
-			else if(charr[i] == '7')
-				res--;
-			else {
-				res = -1;
-				break;
-			}
-		}
-		return res;
-	}
+
+    static void tripleRecursion(int n, int m, int k) {
+        // Complete this function
+    	int[][] matrix = new int[n][n];
+    	init(matrix);
+    	for(int i = 0; i < n; i++)
+    		for(int j = 0; j < n; j++)
+    			matrix[i][j] = getNext(matrix, i, j, m, k);
+    	for(int i = 0; i < n; i++) {
+    		for(int j = 0; j < n; j++)
+    			System.out.print(matrix[i][j] + " ");
+		if(i < n-1)
+    		System.out.println();
+    	}
+    }
+    private static int getNext(int[][] matrix, int i, int j, int m, int k) {
+    	if(matrix[i][j] > Integer.MIN_VALUE)
+    		return matrix[i][j];
+    	if(i == 0 && j == 0)
+    		return m;
+    	else if(i == j)
+    		return matrix[i-1][j-1] + k;
+    	else if (i > j)
+    		return matrix[i-1][j] - 1;
+    	else if (i < j)
+    		return matrix[i][j-1] - 1;
+    	else
+    		return 0;
+    }
+    private static void init(int[][] matrix) {
+    	for(int i = 0; i < matrix.length; i++)
+    		Arrays.fill(matrix[i], Integer.MIN_VALUE);
+    }
+
     public static void main(String[] args) {
-        //Scanner in = new Scanner(System.in);
-        //int n = in.nextInt();
-    	String[] input = {"4",
-    			"HackerBook 777444",
-    			"RankBook 3",
-    			"TheBook 777",
-    			"BestBook 47"};
-    	int n = Integer.parseInt(input[0]);
-    	int min = Integer.MAX_VALUE;
-    	String res = "";
-        for(int a0 = 1; a0 <= n; a0++){
-        	String next = input[a0];
-            String name = next.split(" ")[0];
-            String s = next.split(" ")[1];
-            int value = Integer.parseInt(next.split(" ")[1]);
-            if(count47(value) == 0)
-            	if(min > value) {
-            		min = value;
-            		res = name;
-            	}
-        }
-        
-        if(res != "")
-        	System.out.println(res);
-        else
-        	System.out.println("-1");
+        /*Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int k = in.nextInt();*/
+    	int n = 5;
+        int m = 10;
+        int k = 7;
+        tripleRecursion(n, m, k);
         //in.close();
     }
 }
