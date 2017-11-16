9f7c8b4fe60df30a3cf7659dce21e4657032a015
Mila Klimkina <klimkina@gmail.com>
2017-11-16 08:13:41 -0800

    week code 3

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,3 +1,9 @@
+//Madison, is a little girl who is fond of toys. 
+//Her friend Mason works in a toy manufacturing factory . 
+//Mason has a 2D board  of size  with  rows and  columns. 
+//The board is divided into cells of size  with each cell indicated by it's coordinate . 
+//The cell  has an integer  written on it. To create the toy Mason stacks number of cubes of size  on the cell .
+//Given the description of the board showing the values of  and that the price of the toy is equal to the 3d surface area find the price of the toy.
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -6,48 +12,48 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static void tripleRecursion(int n, int m, int k) {
+    static int surfaceArea(int[][] A) {
         // Complete this function
-    	int[][] matrix = new int[n][n];
-    	init(matrix);
-    	for(int i = 0; i < n; i++)
-    		for(int j = 0; j < n; j++)
-    			matrix[i][j] = getNext(matrix, i, j, m, k);
-    	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j < n; j++)
-    			System.out.print(matrix[i][j] + " ");
-		if(i < n-1)
-    		System.out.println();
-    	}
-    }
-    private static int getNext(int[][] matrix, int i, int j, int m, int k) {
-    	if(matrix[i][j] > Integer.MIN_VALUE)
-    		return matrix[i][j];
-    	if(i == 0 && j == 0)
-    		return m;
-    	else if(i == j)
-    		return matrix[i-1][j-1] + k;
-    	else if (i > j)
-    		return matrix[i-1][j] - 1;
-    	else if (i < j)
-    		return matrix[i][j-1] - 1;
-    	else
+    	if(A.length < 1)
     		return 0;
+    	int res = 0;
+    	for(int i = 0; i < A.length; i++)
+    		for(int j = 0; j < A[0].length; j++) {
+    			res += A[i][j] * 4 + 2 - neighbours(A, i, j);
+    		}
+    	return res;
     }
-    private static void init(int[][] matrix) {
-    	for(int i = 0; i < matrix.length; i++)
-    		Arrays.fill(matrix[i], Integer.MIN_VALUE);
+    static int neighbours(int[][] A, int i, int j) {
+    	int dx[] = {0, 0, -1, 1};
+		int dy[] = {-1, 1, 0, 0};
+		
+		int res = 0;
+		
+		for(int k = 0; k < 4; k++) {
+			int new_x = i + dx[k];
+			int new_y = j + dy[k];
+			if (new_x >= 0 && new_x < A.length
+					&& new_y >= 0 && new_y < A[0].length) 
+				res += Math.min(A[i][j], A[new_x][new_y]);			
+		}		  
+		
+		return res;
     }
-
     public static void main(String[] args) {
         /*Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int k = in.nextInt();*/
-    	int n = 5;
-        int m = 10;
-        int k = 7;
-        tripleRecursion(n, m, k);
+        int H = in.nextInt();
+        int W = in.nextInt();
+        int[][] A = new int[H][W];
+        for(int A_i = 0; A_i < H; A_i++){
+            for(int A_j = 0; A_j < W; A_j++){
+                A[A_i][A_j] = in.nextInt();
+            }
+        }*/
+    	int[][] A = {{1, 3, 4},
+    			{2, 2, 3},
+    			{1, 2, 4}};
+        int result = surfaceArea(A);
+        System.out.println(result);
         //in.close();
     }
 }
