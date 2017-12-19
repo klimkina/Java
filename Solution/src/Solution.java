d697a3452665ba284de672bd42acfd58610849da
Klimkina Liudmila <klimkina@gmail.com>
2017-12-18 18:03:03 -0800

    add numbers list

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,45 +1,68 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
-public class Solution {
-	static long countArray(int n, int k, int x) {
-        // Return the number of ways to fill in the array.
-		
-		return (long)Math.pow(k-1, n-3) * (k-2) * 2 - 1;
-    }
-
-    public static void main(String[] args) {
-    	Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int q = in.nextInt();
-        int[] A = new int[n];
-        int[] rep = new int[n];
-        Arrays.fill(rep, 0);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int start = 0;
-        for(int A_i = 0; A_i < n; A_i++){
-            A[A_i] = in.nextInt();
-            if(map.containsKey(A[A_i])) {
-            	for(; start <= map.get(A[A_i]); start++)
-            		rep[start] = A_i - start;
-            }
-            map.put(A[A_i], A_i);
-        }
-        for(; start < n; start++)
-    		rep[start] = n - start;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+	public static class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; }
+		      ListNode(int[] x) {
+		    	  val = x[0];
+		    	  if(x.length == 1)
+		    		  return;
+		    	  ListNode prev = new ListNode(x[1]);
+		    	  next = prev;
+		    	  for(int i = 2; i < x.length; i++) {
+		    		  ListNode temp = new ListNode(x[i]);
+		    		  prev.next = temp;
+		    		  prev = temp;
+		    	  }
+		      }
+		      public String toString() { 
+		    	  StringBuilder res = new StringBuilder();
+		    	  res.append(val);
+		    	  ListNode temp = next;
+		    	  while(temp != null) {
+		    		  res.append(temp.val);
+		    		  temp = temp.next;
+		    	  }
+		    	  return res.toString();
+		    	} 
+		  }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    	int res = l1.val + l2.val;
+    	int mem = res /  10;
+    	ListNode first = new ListNode(res % 10);
+    	ListNode prev = first;
+    	l1 = l1.next;
+		l2 = l2.next;
+    	while(l1 != null || l2 != null || mem > 0) {
+    		res = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + mem % 10;
+    		mem = mem/10 + res/10;
+    		ListNode next = new ListNode(res % 10);
+    		prev.next = next;
+    		prev = next;
+    		if(l1 != null)
+    			l1 = l1.next;
+    		if (l2 != null)
+    			l2 = l2.next;
+    	}
+    
+    	return first;
         
-        for(int a0 = 0; a0 < q; a0++){
-            int l = in.nextInt();
-            int r = in.nextInt();
-            // Write Your Code Here
-            int res = 0;
-            for(int i = l - 1; i < r; i++)
-        		res += Math.min(rep[i], r - i);
-            System.out.println(res);
-        }
-        in.close();
+    }
+    public static void main(String[] args) {
+    	Solution obj = new Solution();
+    	int[] x1 = {5};
+    	int[] x2 = {5};
+    	ListNode l1 = new ListNode(x1);
+    	ListNode l2 = new ListNode(x2);
+    	System.out.println(obj.addTwoNumbers(l1, l2));
+    	
     }
 }
