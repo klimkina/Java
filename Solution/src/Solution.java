a7f9fce99cefe8d696fd5a860265e5a19e93e21c
Klimkina Liudmila <klimkina@gmail.com>
2017-12-21 18:19:53 -0800

    kth smallest in bst

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,31 +1,53 @@
-
+import java.util.Stack;
 
 class Solution {
-	public int myAtoi(String str) throws NumberFormatException{ 
-		str = str.trim();
-		if(str.isEmpty())
-			return 0;
-		char[] chars = str.toCharArray();
-		boolean isNeg = chars[0] == '-';
-		int res = 0;
-		int start = (isNeg || chars[0] == '+'? 1:0);
-		long temp = 0;
-		for(; start < chars.length; start++) {
-			if(chars[start] < '0' || chars[start] > '9')
-				break;
-			temp = temp*10 + (chars[start] - '0');
-			if(temp > Integer.MAX_VALUE) //operation overflows
-				return isNeg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-			res = (int)temp;
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+		TreeNode(int[] x) { 
+			val = x[0];
+			for(int i = 1; i < x.length; i++)
+				insert(x[i]);
 		}
-		if(isNeg)
-			res = -res;
-        return res;
+		private void insert(int val) {
+			if(val > this.val)
+				if (right == null)
+					right = new TreeNode(val);
+				else
+					right.insert(val);
+			else if (val < this.val)
+				if(left == null)
+					left = new TreeNode(val);
+				else
+					left.insert(val);
+		}
+	}
+	public int kthSmallest(TreeNode root, int k) {
+		Stack<TreeNode> stack = new Stack<>();
+		
+		int visited = 1;
+		while(!(stack.isEmpty() && root == null)) {
+			if(root != null) {
+				stack.push(root);
+				root = root.left;
+			}
+			else {
+				root = stack.pop();
+				if(visited++ == k)
+					return root.val;
+				root = root.right;
+			}
+		} 
+        return 0;
     }
     public static void main(String[] args) {
     	Solution obj = new Solution();
-    	String s = "2147483648";
-    	System.out.println(obj.myAtoi(s));
+    	int[] arr = {5, 2, 7, 1, 3, 6, 8 };
+    	TreeNode tree = new TreeNode(arr);
+    	int k = 5;
+    	System.out.println(obj.kthSmallest(tree, k));
     	
     }
 }
