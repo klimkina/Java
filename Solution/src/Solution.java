d5e477201ec14705c5a1f6c26e0edfebb56bbcfa
Klimkina Liudmila <klimkina@gmail.com>
2017-12-23 18:32:04 -0800

    regex

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,30 +1,51 @@
+import java.util.Stack;
+
 class Solution {
-	public boolean isPalindrome(int x) {
-		if(x < 0)
-			return false;
-		if(x == 0)
-			return true;
-		int l_mask = 1 << 31;
-		int r_mask = 1;
-		while((x & l_mask) == 0){ //most significant left bit
-		    l_mask >>>= 1; 
+	private static class MatchState {
+		int sStart;
+		int pStart;
+		public MatchState(int s, int p) {
+			sStart = s;
+			pStart = p;
 		}
-		while((x & r_mask) == 0){ //most significant right bit
-		    r_mask <<= 1;
+	}
+	public boolean isMatch(String s, String p) {
+		Stack<MatchState> stack = new Stack<>();
+		MatchState curr = null;
+		stack.add(new MatchState(0,0));
+		while(!stack.isEmpty()) {
+			curr = stack.pop();
+			if(curr.sStart >= s.length())
+				if(curr.pStart >= p.length() || p.charAt(curr.pStart) == '*')
+					return true;
+			
+			if(curr.sStart >= s.length() || curr.pStart >= p.length())
+				continue;
+			char currS = s.charAt(curr.sStart);
+			char currP = p.charAt(curr.pStart);
+			if (curr.pStart < p.length()-1 && p.charAt(curr.pStart+1) == '*') // check if next pattern is *
+				stack.push(new MatchState(curr.sStart, curr.pStart+1)); //skip pattern
+			
+			if(currP == '.') // skip both
+				stack.push(new MatchState(curr.sStart+1, curr.pStart+1));
+			else if (currP != '*') {
+				if(currS == currP) // skip both
+					stack.push(new MatchState(curr.sStart+1, curr.pStart+1));
+				
+			} else { // * in pattern
+				currP = p.charAt(curr.pStart-1);
+				if(currP == '.' || currS == currP) 
+					stack.push(new MatchState(curr.sStart+1, curr.pStart));
+				stack.push(new MatchState(curr.sStart, curr.pStart+1));
+			}
 		}
-		while(l_mask >= r_mask){
-		    if((x & l_mask) != 0 && (x & r_mask) == 0
-		    		|| (x & l_mask) == 0 && (x & r_mask) != 0)
-		    	return false;		    	
-		    r_mask <<= 1;
-		    l_mask >>>= 1;
-		}
-        return true;
+        return false;
     }
     public static void main(String[] args) {
     	Solution obj = new Solution();
-    	int k = 10;
-    	System.out.println(obj.isPalindrome(k));
+    	String s = "aa";
+    	String p = ".*";
+    	System.out.println(obj.isMatch(s, p));
     	
     }
 }
