87d52d46d88cc1f22db453245bca4f323f10eb40
Klimkina Liudmila <klimkina@gmail.com>
2018-01-04 13:31:16 -0800

    threesum

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,21 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 class Solution {
-	public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length < 1)
-        	return "";
-        String pre = strs[0];
-        int i = 1;
-        while(i < strs.length){
-            while(strs[i].indexOf(pre) != 0)
-                pre = pre.substring(0,pre.length()-1);
-            i++;
-        }
-        return pre;
+	public List<List<Integer>> threeSum(int[] nums) {
+	        List<List<Integer>> result = new ArrayList<>();
+	        if(nums.length < 3) return result;
+	        Arrays.sort(nums);
+	        int i = 0;
+	        while(i < nums.length - 2) {
+	            if(nums[i] > 0) break;
+	            int j = i + 1;
+	            int k = nums.length - 1;
+	            while(j < k) {
+	                int sum = nums[i] + nums[j] + nums[k];
+	                if(sum == 0) result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+	                if(sum <= 0) while(nums[j] == nums[++j] && j < k);
+	                if(sum >= 0) while(nums[k--] == nums[k] && j < k);
+	            }
+	            while(nums[i] == nums[++i] && i < nums.length - 2);
+	        }
+	        return result;
     }
 	
     public static void main(String[] args) {
-    	String[] strs = {"abcslgfd", "abcfgsa", "abcasbnn"};
+    	int[] nums = {-1, 0, 1, 2, -1, -4};
     	Solution obj = new Solution();
     	
-    	System.out.println(obj.longestCommonPrefix(strs));
+    	List<List<Integer>> res = obj.threeSum(nums);
+    	Iterator itr = res.iterator();
+    	while (itr.hasNext()) {
+    		Iterator inner = ((List<Integer>)itr.next()).iterator();
+    		while(inner.hasNext()) {
+    			System.out.print(inner.next() + " ");
+    		}
+    		System.out.println();
+        }
     }
 }
