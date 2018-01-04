00b88230a87669e4d16173da22429345ecff9278
Klimkina Liudmila <klimkina@gmail.com>
2018-01-04 12:46:45 -0800

    from roman

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -13,10 +13,47 @@ class Solution {
 	    }
 	    return sb.toString();
     }
-	
+	public int romanToInt(String s) {
+		int nums[]=new int[s.length()];
+	    for(int i=0;i<s.length();i++){
+	        switch (s.charAt(i)){
+	            case 'M':
+	                nums[i]=1000;
+	                break;
+	            case 'D':
+	                nums[i]=500;
+	                break;
+	            case 'C':
+	                nums[i]=100;
+	                break;
+	            case 'L':
+	                nums[i]=50;
+	                break;
+	            case 'X' :
+	                nums[i]=10;
+	                break;
+	            case 'V':
+	                nums[i]=5;
+	                break;
+	            case 'I':
+	                nums[i]=1;
+	                break;
+	        }
+	    }
+	    int sum=0;
+	    for(int i=0;i<nums.length-1;i++){
+	        if(nums[i]<nums[i+1])
+	            sum-=nums[i];
+	        else
+	            sum+=nums[i];
+	    }
+	    return sum+nums[nums.length-1];
+    }
     public static void main(String[] args) {
     	int num = 1235;
     	Solution obj = new Solution();
-    	System.out.println(obj.intToRoman(num));
+    	String s = obj.intToRoman(num);
+    	System.out.println(s);
+    	System.out.println(obj.intToRoman(s));
     }
 }
