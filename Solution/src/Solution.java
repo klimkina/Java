30a94051b878310faa8e078f2030f7b13a536e95
Klimkina Liudmila <klimkina@gmail.com>
2018-01-06 14:34:47 -0800

    longest file path

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,46 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
 class Solution {
 	
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-    	List<Integer> res = new ArrayList<>();
-    	 if(nums.length < 2)
-             return res;
-         else{
-             Arrays.sort(nums);
-             int[] parent = new int[nums.length];
-             int[] count = new int[nums.length];
-             int max = 0, maxind = -1;
-             for(int i = nums.length - 1; i >= 0; i--){
-                 for(int j = i; j < nums.length; j++){
-                     if(nums[j] % nums[i] == 0 && count[i] < 1 + count[j] ){
-                         count[i] = 1 + count[j];
-                         parent[i] = j;
-                         if(count[i] > max){
-                             max = count[i];
-                             maxind = i;
-                         }
-                     }
-                 }
-             }
-             for(int i = 0; i < max; i++){
-                 res.add(nums[maxind]);
-                 maxind = parent[maxind];
-             }
-             return res;
-         }
+	public int lengthLongestPath(String input) {
+		
+		input = input.replace("\\n", "\n");
+		input = input.replace("\\t", "\t");
+		String[] paths = input.split("\\n");
+	    int[] stack = new int[paths.length+1];
+	    int maxLen = 0;
+	    for(String s:paths){
+	        int lev = s.lastIndexOf("\t")+1;
+	        stack[lev+1] = stack[lev]+s.length()-(lev)+1; // remove "\\t", add"\"
+	        if(s.contains(".")) 
+	        	maxLen = Math.max(maxLen, stack[lev+1]-1);
+	    }
+	    return maxLen;
    }
 
     public static void main(String[] args) {
-    	int[] nums = {2,3,8,9,27};
+    	String file_sys = "dir\\n\\tsubdir1\\n\\t\\tfile1.ext\\n\\t\\tsubsubdir1\\n\\tsubdir2\\n\\t\\tsubsubdir2\\n\\t\\t\\tfile2.ext";
     	Solution obj = new Solution();
     	
-    	List<Integer> res = obj.largestDivisibleSubset(nums);
-    	for(Integer i:res)
-    		System.out.print(i + " ");
+    	System.out.print(obj.lengthLongestPath(file_sys));
         
     }
 }
