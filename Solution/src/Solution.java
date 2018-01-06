b0a9864d9ab8f9400407d2bc5d9443e2a99bf38e
Klimkina Liudmila <klimkina@gmail.com>
2018-01-05 16:13:26 -0800

    divisible subs dp fast

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,46 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 class Solution {
+	
     public List<Integer> largestDivisibleSubset(int[] nums) {
-    	if(nums == null || nums.length == 0)
-    		return new ArrayList<>();
-        List<List<Integer>> subs = new ArrayList<>();
-        int maxId = 0;
-        boolean inserted;
-        boolean divisible;
-        for(int i = 0; i < nums.length; i++) {
-        	inserted = false;
-        	List<Integer> l = new ArrayList<>();
-        	for(int j = 0; j < subs.size(); j++) {
-        		divisible = true;
-        		for(Integer num : subs.get(j)) {
-        			if(Math.max(nums[i], num)%Math.min(nums[i], num) != 0) 
-        				divisible = false;
-        			else
-        				l.add(num);
-        		}
-        		if(divisible) {
-        			inserted = true;
-        			subs.get(j).add(nums[i]);
-        			if(subs.get(maxId).size() < subs.get(j).size())
-        				maxId = j;
-        		}
-        	}
-        	if(!inserted) {
-        		l.add(Integer.valueOf(nums[i]));
-        		if(!subs.isEmpty() && subs.get(maxId).size() < l.size())
-    				maxId = subs.size();
-        		subs.add(l);
-        	}
-        }
-        return subs.get(maxId);
-    }
-
-   
+    	List<Integer> res = new ArrayList<>();
+    	 if(nums.length < 2)
+             return res;
+         else{
+             Arrays.sort(nums);
+             int[] parent = new int[nums.length];
+             int[] count = new int[nums.length];
+             int max = 0, maxind = -1;
+             for(int i = nums.length - 1; i >= 0; i--){
+                 for(int j = i; j < nums.length; j++){
+                     if(nums[j] % nums[i] == 0 && count[i] < 1 + count[j] ){
+                         count[i] = 1 + count[j];
+                         parent[i] = j;
+                         if(count[i] > max){
+                             max = count[i];
+                             maxind = i;
+                         }
+                     }
+                 }
+             }
+             for(int i = 0; i < max; i++){
+                 res.add(nums[maxind]);
+                 maxind = parent[maxind];
+             }
+             return res;
+         }
+   }
 
     public static void main(String[] args) {
-    	int[] nums = {1,2,3};
+    	int[] nums = {2,3,8,9,27};
     	Solution obj = new Solution();
     	
     	List<Integer> res = obj.largestDivisibleSubset(nums);
