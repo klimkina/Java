a88ab970e1fbfc0c55ed2c5c4e01daf386e3e620
Klimkina Liudmila <klimkina@gmail.com>
2018-01-06 18:34:53 -0800

    walls and gates

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,35 +1,47 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-	public int maxCoins(int[] nums) {
-	    if (nums == null || nums.length == 0) return 0;
-	    
-	    int[][] dp = new int[nums.length][nums.length];
-	    for (int len = 1; len <= nums.length; len++) {
-	        for (int start = 0; start <= nums.length - len; start++) {
-	            int end = start + len - 1;
-	            for (int i = start; i <= end; i++) {
-	                int coins = nums[i] * getValue(nums, start - 1) * getValue(nums, end + 1);
-	                coins += i != start ? dp[start][i - 1] : 0; // If not first one, we can add subrange on its left.
-	                coins += i != end ? dp[i + 1][end] : 0; // If not last one, we can add subrange on its right
-	                dp[start][end] = Math.max(dp[start][end], coins);
-	            }
+	public void wallsAndGates(int[][] rooms) {
+		if (rooms.length == 0 || rooms[0].length == 0) return;
+	    Queue<int[]> queue = new LinkedList<>();
+	    for (int i = 0; i < rooms.length; i++) {
+	        for (int j = 0; j < rooms[0].length; j++) {
+	            if (rooms[i][j] == 0) queue.add(new int[]{i, j});
 	        }
 	    }
-	    return dp[0][nums.length - 1];
-	}
-
-	private int getValue(int[] nums, int i) { // Deal with num[-1] and num[num.length]
-	    if (i < 0 || i >= nums.length) {
-	        return 1;
+	    while (!queue.isEmpty()) {
+	        int[] top = queue.remove();
+	        int row = top[0], col = top[1];
+	        int[] dx = {1, -1, 0, 0};
+	        int[] dy = {0, 0, 1, -1};
+	        
+	        for(int i = 0; i < 4; i++) {
+	        	int new_row = row + dy[i];
+	        	int new_col = col + dx[i];
+	        	if(new_row >= 0 && new_row < rooms.length &&
+	        			new_col >= 0 && new_col < rooms[0].length &&
+	        			rooms[new_row][new_col] == Integer.MAX_VALUE) {
+	        		
+	        		rooms[new_row][new_col] = rooms[row][col] + 1;
+	        		queue.add(new int[]{new_row, new_col});
+	        	}
+	        
+	        }
 	    }
-	    return nums[i];
 	}
     public static void main(String[] args) {
     	Solution obj = new Solution();
-    	int[] coins = {3, 1, 5, 8};
-    	System.out.print(obj.maxCoins(coins));
+    	int[][] house = {{2147483647,  -1,  0,  2147483647},
+    			{2147483647, 2147483647, 2147483647,  -1},
+    			{2147483647,  -1, 2147483647,  -1},
+    			  {0,  -1,2147483647, 2147483647}};
+    	obj.wallsAndGates(house);
+    	for(int row = 0; row < house.length; row++) {
+    		for(int col = 0; col < house[0].length; col++)
+    			System.out.print(house[row][col] + " ");
+    		System.out.println();
+    	}
         
     }
 }
