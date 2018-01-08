a352020de6a8af4fbd06cd4aa5f86e7ad438a970
Klimkina Liudmila <klimkina@gmail.com>
2018-01-07 21:13:52 -0800

    random list node

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,29 +1,42 @@
+import java.util.Random;
+
 class Solution {
-	public boolean isStrobogrammatic(String num) {
-        char[] charr = num.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for(int i = charr.length -1; i >= 0; i--)
-        	sb.append(rotate(charr[i]));
-        return sb.toString().equals(num);
-    }
-	private String rotate(char ch) {
-		switch(ch) {
-			case '0' : return "0";
-			case '1' : return "1";
-			case '6' : return "9";
-			case '8' : return "8";
-			case '9' : return "6";
-			default: return "#";
-		}
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
 	}
+	
+	private int size;
+	private ListNode head;
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.head = head;
+        ListNode curr = head;
+        size = 0;
+        while(curr != null) {
+        	size++;
+        	curr = curr.next;
+        }
+        
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        Random random = new Random();
+        int index = random.nextInt(size);
+        ListNode curr = head;
+        int count = 0;
+        while(curr != null && count < index) {
+        	count++;
+        	curr = curr.next;
+        }
+        return curr.val;
+    }
+	
 	    
     public static void main(String[] args) {   			
     	Solution obj = new Solution();
-    	System.out.println(obj.isStrobogrammatic("666"));
-    	System.out.println(obj.isStrobogrammatic("88"));
-    	System.out.println(obj.isStrobogrammatic("69"));
-    	System.out.println(obj.isStrobogrammatic("618"));
-    	System.out.println(obj.isStrobogrammatic("619"));
-    	System.out.println(obj.isStrobogrammatic("225"));
     }
 }
