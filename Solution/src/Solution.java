2402a3a7b0344eb3b19b52a74bee5938076c7c0b
Klimkina Liudmila <klimkina@gmail.com>
2018-01-07 20:55:49 -0800

    rangeop

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,67 +1,40 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 class Solution {
-	public List<String> findWords(char[][] board, String[] words) {
-	    List<String> res = new ArrayList<>();
-	    TrieNode root = buildTrie(words);
-	    for (int i = 0; i < board.length; i++) {
-	        for (int j = 0; j < board[0].length; j++) {
-	            dfs (board, i, j, root, res);
-	        }
+	public int[] getModifiedArray(int length, int[][] updates) {
+
+	    int[] res = new int[length];
+	     for(int[] update : updates) {
+	        int value = update[2];
+	        int start = update[0];
+	        int end = update[1];
+	        
+	        res[start] += value;
+	        
+	        if(end < length - 1)
+	            res[end + 1] -= value;
+	        
 	    }
+	    
+	    int sum = 0;
+	    for(int i = 0; i < length; i++) {
+	        sum += res[i];
+	        res[i] = sum;
+	    }
+	    
 	    return res;
-	}
-
-	public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
-	    char c = board[i][j];
-	    if (c == '#' || p.next[c - 'a'] == null) return;
-	    p = p.next[c - 'a'];
-	    if (p.word != null) {   // found one
-	        res.add(p.word);
-	        p.word = null;     // de-duplicate
-	    }
-
-	    board[i][j] = '#';
-	    if (i > 0) dfs(board, i - 1, j ,p, res); 
-	    if (j > 0) dfs(board, i, j - 1, p, res);
-	    if (i < board.length - 1) dfs(board, i + 1, j, p, res); 
-	    if (j < board[0].length - 1) dfs(board, i, j + 1, p, res); 
-	    board[i][j] = c;
-	}
-
-	public TrieNode buildTrie(String[] words) {
-	    TrieNode root = new TrieNode();
-	    for (String w : words) {
-	        TrieNode p = root;
-	        for (char c : w.toCharArray()) {
-	            int i = c - 'a';
-	            if (p.next[i] == null) p.next[i] = new TrieNode();
-	            p = p.next[i];
-	       }
-	       p.word = w;
-	    }
-	    return root;
-	}
-
-	class TrieNode {
-	    TrieNode[] next = new TrieNode[26];
-	    String word;
 	}
 	
 	    
     public static void main(String[] args) {
-    	String[] words = {"abbbababaa"};
-    	char[][] board = {{'b','b','a','a','b','a'},{'b','b','a','b','a','a'},
-    			{'b','b','b','b','b','b'},{'a','a','a','b','a','a'},
-    			{'a','b','a','a','b','b'}};
+    	int length = 5;
+    	int[][]  updates = {
+    		        {1,  3,  2},
+    		        {2,  4,  3},
+    		        {0,  2, -2}
+    		        };
     			
     	Solution obj = new Solution();
-    	List<String> strs = obj.findWords(board, words);
-    	for(String s: strs)
-    		System.out.println(s);
+    	int[] res = obj.getModifiedArray(length, updates);
+    	for(int i: res)
+    		System.out.print(i + ", ");
     }
 }
