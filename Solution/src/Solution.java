74c7133fab7553a50f3ea872214291b313eb6009
Klimkina Liudmila <klimkina@gmail.com>
2018-01-09 10:47:30 -0800

    next Larger int swapping digits

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,59 +1,31 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 class Solution {
-	private Set<Integer>[] adj;
-	private boolean[] marked;
-	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-		List<Integer> res = new ArrayList<>();
-		if(n == 1) {
-			res.add(0);
-			return res;
+	public int nextLarger(int num){
+		String s = "" + num;
+		char[] charr = s.toCharArray();
+		for(int i = charr.length-1; i > 0; i--) {
+			if(charr[i-1] < charr[i]) {
+				return Integer.parseInt(helper(charr, i-1));
+			}
 		}
-		marked = new boolean[n];
-		adj = (Set<Integer>[])new HashSet[n];
-		for(int i = 0; i < edges.length; i++) {
-			if(adj[edges[i][0]] == null)
-				adj[edges[i][0]] = new HashSet();
-			if(adj[edges[i][1]] == null)
-				adj[edges[i][1]] = new HashSet();
-			adj[edges[i][0]].add(edges[i][1]);
-			adj[edges[i][1]].add(edges[i][0]);
-		}
-		int[] degree = new int[n];  //degree
-        for(int i=0; i<n; i++) 
-        	degree[i]=adj[i].size();
-        
-        for(int remain=n, j; remain>2;){
-            ArrayList<Integer> del = new ArrayList<>(); // nodes to delete
-            for(j=0; j<n; j++){
-                if(degree[j]==1) { //find leaves
-                    remain--;
-                    del.add(j);
-                    degree[j]=-1;
-                }
-            }
-            for(int k: del){ //delete this node and its edges 
-                for(int neigh: adj[k]) 
-                	degree[neigh]--;
-            }
-        }
-        for(int i=0; i<n; i++) 
-        	if(degree[i]>=0) res.add(i);
-        return res;
-    }
+		return -1;
+	}
 	
-	    
+	private String helper(char[] charr, int start) {
+		int next_larger_id =start+1;
+		for(int i = start+2; i < charr.length - 1; i++)
+			if(charr[i] > charr[start] && charr[i] < charr[next_larger_id])
+				next_larger_id = i;
+		char temp = charr[start];
+		charr[start] = charr[next_larger_id];
+		charr[next_larger_id] = temp;
+		Arrays.sort(charr, start+1, charr.length-1);
+		return String.valueOf(charr);
+	}
+    
     public static void main(String[] args) {   	
-    	int[][] edges = {{0, 1}};
     	Solution obj = new Solution();
-    	List<Integer> res = obj.findMinHeightTrees(2, edges);
-    	if(res != null)
-	    	for(int i : res)
-	    		System.out.print(i + " ");
+    	System.out.print(obj.nextLarger(8142));
     }
 }
