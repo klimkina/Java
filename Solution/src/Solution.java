a871acb9246c87b2b3fbf187beac2c7f3f77ca77
Klimkina Liudmila <klimkina@gmail.com>
2018-01-08 16:33:47 -0800

    Minimum Height Trees

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,25 +1,57 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
-	
-	public boolean isReflected(int[][] points) {
-		if(points.length < 2)
-			return true;
-        Arrays.sort(points, (a,b)->(a[0] - b[0]));
-        HashSet<String> set = new HashSet<>();
-        for(int i = 0; i < points.length; i++)
-        	set.add(points[i][0] + "#" + points[i][1]);
-        int y = points[0][0] + points[points.length-1][0];
-        for(int i = 0; i < points.length / 2; i++)
-        	if(!set.contains(y - points[i][0] + "#" + points[i][1]))
-        		return false;
-        return true;
+	private Set<Integer>[] adj;
+	private boolean[] marked;
+	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+		List<Integer> res = new ArrayList<>();
+		marked = new boolean[n];
+		adj = (Set<Integer>[])new HashSet[n];
+		for(int i = 0; i < edges.length; i++) {
+			if(adj[edges[i][0]] == null)
+				adj[edges[i][0]] = new HashSet();
+			if(adj[edges[i][1]] == null)
+				adj[edges[i][1]] = new HashSet();
+			adj[edges[i][0]].add(edges[i][1]);
+			adj[edges[i][1]].add(edges[i][0]);
+		}
+		int min = Integer.MAX_VALUE;
+		int[] heights = new int[n];
+		for(int i = 0; i < n; i++) {
+			Arrays.fill(marked, false);
+			heights[i] = findHeight(i);
+			if(heights[i] < min)
+				min = heights[i];
+		}
+		for(int i = 0; i < n; i++)
+			if(heights[i] == min)
+				res.add(i);
+        return res;
     }
+	private int findHeight(int i) { // dfs
+		marked[i] = true;
+		int max = 0;
+		int temp;
+		for(int v: adj[i])
+			if(!marked[v]) {
+				temp = findHeight(v) + 1;
+				if(max < temp)
+					max = temp;
+			}
+		return max;
+	}
 	    
     public static void main(String[] args) {   	
-    	int[][] points = {{-16,1},{16,1},{16,1}};
+    	int[][] edges = {{0, 1}};
     	Solution obj = new Solution();
-    	System.out.println(obj.isReflected(points));
+    	List<Integer> res = obj.findMinHeightTrees(2, edges);
+    	if(res != null)
+	    	for(int i : res)
+	    		System.out.print(i + " ");
     }
 }
