f189a79ca2deaeefd2ba71ac9a6cd6ed74ccaa32
Klimkina Liudmila <klimkina@gmail.com>
2018-01-15 18:54:34 -0800

    Prime Number of Set Bits in Binary Representation

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,31 +1,34 @@
-import java.util.Arrays;
+/*Given two integers L and R, find the count of numbers in the range [L, R] (inclusive) having a prime number of set bits in their binary representation.
 
+(Recall that the number of set bits an integer has is the number of 1s present when written in binary. For example, 21 written in binary is 10101 which has 3 set bits. Also, 1 is not a prime.)
+*/
 class Solution {
-	public int nextLarger(int num){
-		String s = "" + num;
-		char[] charr = s.toCharArray();
-		for(int i = charr.length-1; i > 0; i--) {
-			if(charr[i-1] < charr[i]) {
-				return Integer.parseInt(helper(charr, i-1));
-			}
+	public int countPrimeSetBits(int L, int R) {
+		int res = 0;
+		for(int i = L; i <= R; i++)
+			if(isPrime(countBits(i)))
+				res++;
+        return res;
+    }
+	private int countBits(int num) {
+		int res = 0;
+		while(num != 0) {
+			if((num&1) != 0)
+				res++;
+			num >>>= 1;
 		}
-		return -1;
+		return res;
 	}
-	
-	private String helper(char[] charr, int start) {
-		int next_larger_id =start+1;
-		for(int i = start+2; i < charr.length - 1; i++)
-			if(charr[i] > charr[start] && charr[i] < charr[next_larger_id])
-				next_larger_id = i;
-		char temp = charr[start];
-		charr[start] = charr[next_larger_id];
-		charr[next_larger_id] = temp;
-		Arrays.sort(charr, start+1, charr.length-1);
-		return String.valueOf(charr);
+	private boolean isPrime(int num) {
+		if(num < 2) return false;
+		for(int i = 2; i <= Math.sqrt(num); i++)
+			if(num % i == 0)
+				return false;
+		return true;
 	}
     
     public static void main(String[] args) {   	
     	Solution obj = new Solution();
-    	System.out.print(obj.nextLarger(8142));
+    	System.out.print(obj.countPrimeSetBits(6, 10));
     }
 }
