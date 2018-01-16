88e2f421e7d69ba1bd4f84933bf8babf4842e9b8
Klimkina Liudmila <klimkina@gmail.com>
2018-01-16 14:38:31 -0800

    orderOfLargestPlusSign

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -38,14 +38,14 @@ class Solution {
 		for (int i = 0; i < N; i++)
             for (int j=0; j < N; j++) 
                 if (matrix[i][j] == 1) {
-                    res[i][j][0] = (j == 0 ? 1 : res[i][j-1][0] + 1);
-                    res[i][j][1] = (i == 0 ? 1 : res[i-1][j][1] + 1);
+                    res[i][j][0] = (j == 0 ? 1 : res[i][j-1][0] + 1);//left
+                    res[i][j][1] = (i == 0 ? 1 : res[i-1][j][1] + 1);//up
                 }
         for (int i = N-1; i >= 0; i--)
             for (int j = N-1; j >= 0; j--) 
                 if (matrix[i][j] == 1) {
-                	res[i][j][2] = (j == N-1 ? 1 : res[i][j+1][2] + 1);
-                	res[i][j][3] = (i == N-1 ? 1 : res[i+1][j][3] + 1);
+                	res[i][j][2] = (j == N-1 ? 1 : res[i][j+1][2] + 1);//right
+                	res[i][j][3] = (i == N-1 ? 1 : res[i+1][j][3] + 1);//down
                 }
 	}
     
