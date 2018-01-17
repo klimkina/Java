fff6b31f67034c77972e6f20e765aeae6e0995b6
Klimkina Liudmila <klimkina@gmail.com>
2018-01-17 15:37:45 -0800

    int b0 = -prices[0], b1 = b0;

--- Solution/src/MaxProfit.java
+++ Solution/src/MaxProfit.java
@@ -7,17 +7,17 @@ After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day
 */
 public class MaxProfit {
 	public int maxProfit(int[] prices) {
-		if(prices == null || prices.length == 0)
+		if(prices == null || prices.length <= 1)
 			return 0;
-        int[] buy = new int[prices.length + 1];
-        int[] sell = new int[prices.length + 1];
-        buy[0] = -prices[0];
-        sell[0] = 0;
-        for(int i = 1; i < prices.length; i++) {
-        	buy[i] = Math.max(buy[i-1], (i > 1 ? sell[i-2] - prices[i] : -prices[1]));
-        	sell[i] = Math.max(sell[i-1], buy[i-1] + prices[i]);
-        }
-        return Math.max(buy[prices.length-1], sell[prices.length-1]);
+		int b0 = -prices[0], b1 = b0;
+	    int s0 = 0, s1 = 0, s2 = 0;
+	 
+	    for(int i = 1; i < prices.length; i++) {
+	    	b0 = Math.max(b1, s2 - prices[i]);
+	    	s0 = Math.max(s1, b1 + prices[i]);
+	    	b1 = b0; s2 = s1; s1 = s0; 
+	    }
+	    return s0;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
