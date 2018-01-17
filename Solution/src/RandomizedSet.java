e9bfdb3e80d6dc9e0e3e0cb6153d11c3129fc218
Klimkina Liudmila <klimkina@gmail.com>
2018-01-16 17:00:34 -0800

    randomized set

--- Solution/src/RandomizedSet.java
+++ Solution/src/RandomizedSet.java
@@ -5,40 +5,49 @@
  getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
 */
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Random;
 
 public class RandomizedSet {
 	/** Initialize your data structure here. */
-	private HashSet<Integer> set;
-    public RandomizedSet() {
-        set = new HashSet<>();
-    }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    public boolean insert(int val) {
-    	boolean res = set.contains(val);
-        set.add(val);
-        return !res;
-    }
-    
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    public boolean remove(int val) {
-    	boolean res = set.contains(val);
-        set.remove(val);
-        return res;
-    }
-    
-    /** Get a random element from the set. */
-    public int getRandom() {
-    	Random rand = new Random();
-    	int idx = rand.nextInt(set.size());
-    	List asList = new ArrayList(set);
-    	Collections.shuffle(asList);
-    	return (int)asList.get(0);
-    }
+	    ArrayList<Integer> nums;
+	    HashMap<Integer, Integer> locs;
+	    java.util.Random rand = new java.util.Random();
+	    /** Initialize your data structure here. */
+	    public RandomizedSet() {
+	        nums = new ArrayList<Integer>();
+	        locs = new HashMap<Integer, Integer>();
+	    }
+	    
+	    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+	    public boolean insert(int val) {
+	        boolean contain = locs.containsKey(val);
+	        if ( contain ) return false;
+	        locs.put( val, nums.size());
+	        nums.add(val);
+	        return true;
+	    }
+	    
+	    /** Removes a value from the set. Returns true if the set contained the specified element. */
+	    public boolean remove(int val) {
+	        boolean contain = locs.containsKey(val);
+	        if ( ! contain ) return false;
+	        int loc = locs.get(val);
+	        if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
+	            int lastone = nums.get(nums.size() - 1 );
+	            nums.set( loc , lastone );
+	            locs.put(lastone, loc);
+	        }
+	        locs.remove(val);
+	        nums.remove(nums.size() - 1);
+	        return true;
+	    }
+	    
+	    /** Get a random element from the set. */
+	    public int getRandom() {
+	        return nums.get( rand.nextInt(nums.size()) );
+	    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Init an empty set.
