38cfb3c10bccb8ad4b85a2bc6d893601b848b347
Klimkina Liudmila <klimkina@gmail.com>
2018-01-17 14:02:22 -0800

    pow easy

--- Solution/src/Pow.java
+++ Solution/src/Pow.java
@@ -1,14 +1,13 @@
 
 public class Pow {
 	public double myPow(double x, int n) {
-        if(n == 0)
-        	return 1;
-        if(n == 1)
-        	return x;
-        if (n == -1)
-        	return 1/x;
-        double temp = myPow(x, n/2);
-        return temp*temp*(n%2 == 1 || n%2 == -1 ? (n > 0? x : 1/x) : 1);
+		if(n == 0)
+            return 1;
+        if(n < 0){
+            n = -n;
+            x = 1/x;
+        }
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
