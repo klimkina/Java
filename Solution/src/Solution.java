f93a94d98e71e20b96fe4960d07777abce02aa4c
Klimkina Liudmila <klimkina@gmail.com>
2018-01-18 13:48:17 -0800

    Find the Duplicate Number

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,29 +1,38 @@
-/*Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+/*Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
 
-(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
-
-Find the minimum element.
-
-You may assume no duplicate exists in the array.*/
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.*/
 
 class Solution {
-	public int findMin(int[] nums) {
-        int low = 0, high = nums.length - 1;
-        int mid;
-        while(low < high) {
-        	mid = low + (high - low)/2;
-        	if(nums[mid] < nums[high]) 
-        		high = mid; // min is at the left        	
-        	else
-        		low = mid + 1; // min is at the right
-        }
-        return nums[low];
+	public int findDuplicate(int[] nums) {
+		if (nums.length > 1)
+		{
+			int slow = nums[0];
+			int fast = nums[nums[0]];
+			while (slow != fast) // slow meets fast in a loop
+			{
+				slow = nums[slow];
+				fast = nums[nums[fast]];
+			}
+
+			fast = 0; // set to the beginning so they meet at the beginning of the loop
+			while (fast != slow)
+			{
+				fast = nums[fast];
+				slow = nums[slow];
+			}
+			return slow;
+		}
+		return -1;
     }
     
 	
     public static void main(String[] args) { 
     	Solution obj = new Solution();
-    	int[] nums = {4, 5, 6, 7, 0, 1, 2};
-    	System.out.println(obj.findMin(nums));
+    	int[] nums = {2, 2, 2, 2, 2};
+    	System.out.println(obj.findDuplicate(nums));
     }
 }
