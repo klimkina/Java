40e03fbaae83b5cd4f107f71d39661f1ed904089
Klimkina Liudmila <klimkina@gmail.com>
2018-01-18 16:06:09 -0800

    Shortest Palindrome

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,38 +1,41 @@
-/*Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+import java.util.ArrayList;
 
-Note:
-You must not modify the array (assume the array is read only).
-You must use only constant, O(1) extra space.
-Your runtime complexity should be less than O(n2).
-There is only one duplicate number in the array, but it could be repeated more than once.*/
+/*Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. Find and return the shortest palindrome you can find by performing this transformation.
+
+For example:
+
+Given "aacecaaa", return "aaacecaaa".
+
+Given "abcd", return "dcbabcd".*/
 
 class Solution {
-	public int findDuplicate(int[] nums) {
-		if (nums.length > 1)
-		{
-			int slow = nums[0];
-			int fast = nums[nums[0]];
-			while (slow != fast) // slow meets fast in a loop
-			{
-				slow = nums[slow];
-				fast = nums[nums[fast]];
-			}
-
-			fast = 0; // set to the beginning so they meet at the beginning of the loop
-			while (fast != slow)
-			{
-				fast = nums[fast];
-				slow = nums[slow];
-			}
-			return slow;
-		}
-		return -1;
+	public String shortestPalindrome(String s) {
+		if(s == null || s.isEmpty() || s.length() == 1)
+			return s;
+		ArrayList<Character> pref = new ArrayList<>();
+		char[] charr = s.toCharArray();
+		int count = 1;
+		while(!isPalindrome(pref, charr)) 
+			pref.add(charr[charr.length - count++]);
+		StringBuilder builder = new StringBuilder(pref.size());
+	    for(Character ch: pref)
+	    {
+	        builder.append(ch);
+	    }
+		return builder.toString() + s;
     }
     
-	
+	private boolean isPalindrome(ArrayList<Character> arr, char[] charr) {
+		for(int i = 0; i < (arr.size() + charr.length) /2; i++)
+			if((i < arr.size() ? arr.get(i) : charr [i - arr.size()]) != charr[charr.length - i - 1])
+				return false;
+		return true;
+	}
     public static void main(String[] args) { 
     	Solution obj = new Solution();
-    	int[] nums = {2, 2, 2, 2, 2};
-    	System.out.println(obj.findDuplicate(nums));
+    	String s1 = "aacecaaa";
+    	String s2 = "abcd";
+    	System.out.println(obj.shortestPalindrome(s1));
+    	System.out.println(obj.shortestPalindrome(s2));
     }
 }
