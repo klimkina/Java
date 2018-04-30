3c118e274740b9dda3fd528f9ef3a6f5f735da78
Klimkina Liudmila <klimkina@gmail.com>
2018-04-29 19:13:58 -0700

    mix

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -6,33 +6,23 @@ import java.util.regex.*;
 
 public class Solution {
 	
-	public int numFriendRequests(int[] ages) {
-		Arrays.sort(ages);
-		int less = 0;
-		int count = 0;
-		int num = 0;
-		TreeMap<Integer, Integer> hashmap = new TreeMap<>();
-		for(int i = 0; i < ages.length; i++)
+	public int[] mix(int[] arr) {
+		int[] res = Arrays.copyOf(arr, arr.length);
+		int left = 0;
+		int right = res.length - 1;
+		while(left < right)
 		{
-			double dfloor = ((double)ages[i])/2 + 7;
-			int floor = (int)Math.floor(dfloor);
-			if(!hashmap.isEmpty() && hashmap.floorEntry(floor) != null)
-				less = hashmap.floorEntry(floor).getValue() + 1;
+			if(res[left] == 0) left++;
+			else if (res[right] == 1) right--;
 			else
-				less = 0;
-			num += count - less;
-			int k = 1;
-			dfloor = ((double)ages[i])/2 + 7;
-			if(dfloor < ages[i])
-				while( i+k < ages.length && ages[i+k] == ages[i])
-				{
-					num++; k++;
-				}
-			count++;
-			hashmap.put(ages[i], i);
+			{
+				res[left] = 0;
+				res[right] = 1;
+				left++;
+				right--;
+			}
 		}
-		
-        return num;
+        return res;
     }
 
     public static void main(String[] args) {
@@ -49,13 +39,11 @@ public class Solution {
                 queries[queries_i][queries_j] = in.nextInt();
             }
         }*/
-    	int[] arr1 = {73,106,39,6,26,15,30,100,71,35,46,112,6,60,110};
-    	int[] arr2 = {16,17,18};
-    	int[] arr3 = {20,30,100,110,120};
+    	int[] input =  { 0, 1, 0, 1, 0, 0, 1, 1, 1, 0};
     	Solution obj =  new Solution();
-    	System.out.println(obj.numFriendRequests(arr1));
-    	System.out.println(obj.numFriendRequests(arr2));
-    	System.out.println(obj.numFriendRequests(arr3));
-        //in.close();
+    	int[] res = obj.mix(input);
+    	for (int i = 0; i < res.length; i++)
+
+    		System.out.print(res[i] + " ");//in.close();
     }
 }
