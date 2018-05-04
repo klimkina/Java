afa68a8386dd9742cb7aae02598c3af40312a13f
Klimkina Liudmila <klimkina@gmail.com>
2018-05-04 16:09:53 -0700

    regex

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,34 +8,59 @@ import java.util.stream.Stream;
 
 public class Solution {
 	
-	static String compress(String str)
+	static boolean match(String str, String pattern)
     {
-		if(str == null || str.length() < 2)
-			return str;
-        StringBuilder sb = new StringBuilder();
-        char[] charr = str.toCharArray();
-        char prev = charr[0];
-        int count = 1;
-        for(int i = 1; i <= charr.length; i++)
+		int m = pattern.length();
+		int n = str.length();
+		if (m == 0)
+            return (n == 0);
+      
+        // lookup table for storing results of
+        // subproblems
+        boolean[][] lookup = new boolean[n + 1][m + 1];
+      
+        // initailze lookup table to false
+        for(int i = 0; i < n + 1; i++)
+            Arrays.fill(lookup[i], false);
+         
+      
+        // empty pattern can match with empty string
+        lookup[0][0] = true;
+      
+        // Only '*' can match with empty string
+        for (int j = 1; j <= m; j++)
+            if (pattern.charAt(j - 1) == '*')
+                lookup[0][j] = lookup[0][j - 1];
+      
+        // fill the table in bottom-up fashion
+        for (int i = 1; i <= n; i++)
         {
-        	if(i < charr.length && charr[i] == prev)
-        		count++;
-        	else
-        	{
-        		if(count == 1)
-        			sb.append(prev);
-        		else
-        		{
-        			sb.append(count);
-        			sb.append(prev);
-        			
-        		}
-        		count = 1;
-        		if(i < charr.length)
-        			prev = charr[i];
-        	}
+            for (int j = 1; j <= m; j++)
+            {
+                // Two cases if we see a '*'
+                // a) We ignore '*'' character and move
+                //    to next  character in the pattern,
+                //     i.e., '*' indicates an empty sequence.
+                // b) '*' character matches with ith
+                //     character in input
+                if (pattern.charAt(j - 1) == '*')
+                    lookup[i][j] = lookup[i][j - 1] ||
+                                   lookup[i - 1][j];
+      
+                // Current characters are considered as
+                // matching in two cases
+                // (a) current character of pattern is '?'
+                // (b) characters actually match
+                else if (pattern.charAt(j - 1) == '?' ||
+                    str.charAt(i - 1) == pattern.charAt(j - 1))
+                    lookup[i][j] = lookup[i - 1][j - 1];
+      
+                // If characters don't match
+                else lookup[i][j] = false;
+            }
         }
-        return sb.toString();
+      
+        return lookup[n][m];
     }
     public static void main(String[] args) {
         /*Scanner in = new Scanner(System.in);
@@ -51,7 +76,7 @@ public class Solution {
                 queries[queries_i][queries_j] = in.nextInt();
             }
         }*/
-    	String s = "aabbbcabcc";
-    	System.out.println(compress(s));
+    	String s = "abcfpow";
+    	System.out.println(match(s, "abc*"));
     }
 }
