7d56bca37b2f9ce0134e38c8158186f5e16a8a61
Klimkina Liudmila <klimkina@gmail.com>
2018-06-23 21:54:22 -0700

    factorial

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -9,28 +9,15 @@ import java.util.stream.Stream;
 
 public class Solution {
 	
-	public static boolean isAnagramm (String s1, String s2) {
-		if(s1 == null && s2 == null || s1.length() ==0 && s2.length() == 0)
-			return true;
-		if(s1.length() != s2.length())
-			return false;
-		char[] charr = s1.toCharArray();
-		char[] charr2 = s2.toCharArray();
-		HashMap<Character, Integer> map = new HashMap<>();
-		for(int i = 0; i < s1.length(); i++)
-			map.put(charr[i], map.getOrDefault(charr[i], 0) + 1);
-		for(int i = 0; i < s2.length(); i++)
-			if(!map.containsKey(charr2[i]) || map.get(charr2[i]) == 0)
-				return false;
-			else
-				map.put(charr2[i], map.get(charr2[i])-1);
-		return true;
+	public static int Factorial (int n) {
+		int res = 1;
+		for (int i = 2; i <= n; i++)
+			res *= i;
+		return res;
     }
 	
     public static void main(String[] args) {
-        String s1 = "Red fox jumped over the red rock";
-        String s2 = "Red red jumped over the sock rock";
-    	System.out.println(isAnagramm(s1, s2));
+        System.out.println(Factorial(6));
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
     	
