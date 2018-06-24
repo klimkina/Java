6d741d2369aba703a7affce6891a1e3722743afe
Klimkina Liudmila <klimkina@gmail.com>
2018-06-23 21:40:58 -0700

    reverse words

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -9,36 +9,29 @@ import java.util.stream.Stream;
 
 public class Solution {
 	
-	public static void ThreeSum (int[] arr, int n) {
-       Arrays.sort(arr);
-       for(int i = 0; i < arr.length - 2; i++)
-       {
-    	   int sum = n - arr[i];
-    	   int l = i+1;
-    	   int r = arr.length - 1;
-    	   while (l < r)
-    	   {
-    		   if(arr[l] + arr[r] == sum)
-    		   {
-    			   System.out.println(arr[i] + " " + arr[l] + " " + arr[r]);
-    			   l++;
-    			   r--;
-    		   }
-    		   else if(arr[l] + arr[r] < sum)
-    			   l++;
-    		   else
-    			   r--;
-    	   }
-    	   
-       }
+	public static String ReverseWords (String s) {
+		String[] words = s.split(" ");
+		for (int i = 0; i < words.length; i++)
+			words[i] = reverse(words[i]);
+       return reverse(Arrays.stream(words).collect(Collectors.joining(" ")));
     }
+	
+	private static String reverse (String s)
+	{
+		char[] charr = s.toCharArray();
+		for(int i = 0; i < charr.length/2; i++)
+		{
+			char t = charr[i];
+			charr[i] = charr[charr.length - i - 1];
+			charr[charr.length - i - 1] = t;
+		}
+		return String.valueOf(charr);
+	}
 	
 	
     public static void main(String[] args) {
-        int[] arr = {6, 8, 12, 4, 5, 9, 23};
-        int n = 26;
-        ThreeSum(arr, n);
-    	//System.out.println(mirrorReflection(p, q));
+        String s = "Red fox jumped over the hard rock";
+    	System.out.println(ReverseWords(s));
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
     	
