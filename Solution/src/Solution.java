56cf969b82e61c473717c160627c91de23c3c8f1
Klimkina Liudmila <klimkina@gmail.com>
2018-06-23 21:59:27 -0700

    fibb

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -9,15 +9,26 @@ import java.util.stream.Stream;
 
 public class Solution {
 	
-	public static int Factorial (int n) {
-		int res = 1;
-		for (int i = 2; i <= n; i++)
-			res *= i;
-		return res;
+	public static int Fibb (int n) {
+		if(n < 3)
+			return 1;
+		int[] res = new int[2];
+		res[0] = 1; res[1] = 1;
+		int next = 0;
+		System.out.print("1 1 ");
+		for (int i = 3; i <= n; i++)
+		{
+			
+			next = res[0] + res[1];
+			System.out.print(next + " ");
+			res[0] = res[1];
+			res[1] = next;
+		}
+		return next;
     }
 	
     public static void main(String[] args) {
-        System.out.println(Factorial(6));
+        System.out.println(Fibb(8));
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
     	
