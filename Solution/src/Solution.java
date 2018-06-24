587565a04a68f41caddb34a6c3b6fb8e032aa74a
Klimkina Liudmila <klimkina@gmail.com>
2018-06-23 18:46:06 -0700

    buddy strings

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -9,65 +9,35 @@ import java.util.stream.Stream;
 
 public class Solution {
 	
-	public static class SmallChunks
+	private static boolean isSwap(String a, String b)
 	{
-		private char[] charr;
-		int curr = 0;
-		boolean isNLeft = false;
-		public SmallChunks(char[] charr)
+		if(a == null || b == null || a.length() != b.length() || a.length() < 2)
+			return false;
+		char[] charra = a.toCharArray();
+		char[] charrb = b.toCharArray();
+		HashSet<Character> set = new HashSet<>();
+		boolean hasTwo = false;
+		int diff = -1;
+		for (int i = 0; i < charra.length; i++)
 		{
-			this.charr = Arrays.copyOf(charr, charr.length);
-		}
-		public char[] GetChunk(int size)
-		{
-			char[] res = new char[size];
-			if(size < 1)
-				return res;
-			int start = 0;
-			if (isNLeft)
-				res[start++] = '\n';
-			
-			for(int i = start; i < size; i++)
-			{
-				if (curr >= charr.length)
-					return res;
-				if(charr[curr] == '\n')
-				{
-					curr++;
-					res[i] = '\r';
-					if(i < size - 1)
-						res[++i] = '\n';
-					else
-						isNLeft = true;
-				}
+			if(set.contains(charra[i]))
+				hasTwo = true;
+			set.add(charra[i]);
+			if(charra[i] != charrb[i]) {
+				if(diff >= 0)
+					return charrb[i] == charra[diff] && charra[i] == charrb[diff] && 
+					(i == charra.length - 1 || b.endsWith(a.substring(i + 1)));
 				else
-					res[i] = charr[curr++];
+					diff = i;
 			}
-			return res;
 		}
+		return hasTwo;
 	}
 	
     public static void main(String[] args) {
-        /*Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] A = new int[n];
-        for(int A_i = 0; A_i < n; A_i++){
-            A[A_i] = in.nextInt();
-        }
-        int q = in.nextInt();
-        int[][] queries = new int[q][2];
-        for(int queries_i = 0; queries_i < q; queries_i++){
-            for(int queries_j = 0; queries_j < 2; queries_j++){
-                queries[queries_i][queries_j] = in.nextInt();
-            }
-        }*/
-    	char[] charr = {'a', 'b', 'v', '\n', 'a', 'b', 'c', 'f', '\n', 'a', 'b', '\n'};
-    	SmallChunks chuncks = new SmallChunks(charr);
-    	System.out.println(String.valueOf(chuncks.GetChunk(3)));
-    	System.out.println(String.valueOf(chuncks.GetChunk(3)));
-    	System.out.println(String.valueOf(chuncks.GetChunk(3)));
-    	System.out.println(String.valueOf(chuncks.GetChunk(3)));
-    	System.out.println(String.valueOf(chuncks.GetChunk(3)));
+        String a = "aa";
+        String b = "aa";
+    	System.out.println(isSwap(a, b));
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
     	
