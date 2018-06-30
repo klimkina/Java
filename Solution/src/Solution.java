4390a2e90642eaba8d4bd7acefd4f7221576eda2
Klimkina Liudmila <klimkina@gmail.com>
2018-06-30 16:14:21 -0700

    check versions

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,59 +8,24 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static List<String> removeComments(String[] source) {
-        List<String> res = new ArrayList<>();
-        boolean inBlock = false;
-        int block = 0;
-    	int line = 0;
-    	int last = 0;
-    	String temp;
-        for(String s : source)
-        {
-        	temp = "";
-        	if(inBlock)
-        	{
-        		block = s.indexOf("*/");
-        		if(block >= 0)
-        		{
-        			temp = s.substring(block+2, s.length());
-        			inBlock = false;
-        		}
-        	}
-        	else
-        	{
-	        	block = s.indexOf("/*");
-	        	line = s.indexOf("//");
-	        	if(block >= 0 && (line < 0 || block < line))
-	        	{
-	        		last = s.indexOf("*/");
-	        		if(last >= 0)
-	        		{
-	        			temp = s.substring(0, block);
-	        			if (last < s.length() - 2)
-	        				temp += s.substring(last + 2, s.length());
-	        		}
-        			else
-        				inBlock = true;
-	        	}
-	        	
-	        	if (block >= 0 && inBlock || line >= 0)
-	        		temp = (s.substring(0, Math.min(Math.max(0, block), Math.max(0, line))));
-	        	else if (block < 0)
-	        		temp = s;
-	        	if(temp != null && temp != "")
-	        		res.add(temp);
-        	}
+	public static int compareVersion(String version1, String version2) {
+		String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int len = Math.max(v1.length,v2.length);
+        for(int i=0;i<len;i++){
+            int c1 = 0;
+            int c2 = 0;
+            if(v1.length>i) c1 = Integer.parseInt(v1[i]);
+            if(v2.length>i) c2 = Integer.parseInt(v2[i]);            
+            if(c1>c2) return 1;
+            if(c2>c1) return -1;
         }
-        return res;
-    }
+        return 0;
+	}
 	
     public static void main(String[] args) {
-    	String[] input = {"/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"};
-    			
-    	List<String> output = removeComments(input);
-    	for (String s : output)
-    		System.out.println(s);
+    	String version1 = "1.1", version2 = "1";
+    	System.out.println(compareVersion(version1, version2));
     	
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
