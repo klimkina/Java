9dd7c3c0c4f644e44d2c32b6b04c1e455ccd8bbb
Klimkina Liudmila <klimkina@gmail.com>
2018-06-30 19:04:49 -0700

    lemonade change

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,59 +8,54 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	static class MyQueue {
-		private static Stack<Integer> stack;
-		private static Stack<Integer> stack2;
-	    /** Initialize your data structure here. */
-	    public MyQueue() {
-	        stack = new Stack<>();
-	        stack2 = new Stack<>();
-	    }
-	    
-	    private void initStack2()
-	    {
-	    	while(!stack.isEmpty())
-	    		stack2.push(stack.pop());
-	    }
-	    /** Push element x to the back of queue. */
-	    public void push(int x) {
-	        stack.push(x);
-	    }
-	    
-	    /** Removes the element from in front of queue and returns that element. */
-	    public int pop() {
-	        if(stack2.isEmpty())
-	        	initStack2();
-        	return stack2.pop();
-	    }
-	    
-	    /** Get the front element. */
-	    public int peek() {
-	    	if(stack2.isEmpty())
-	        	initStack2();
-        	return stack2.peek();
-	    }
-	    
-	    /** Returns whether the queue is empty. */
-	    public boolean empty() {
-	        return stack2.isEmpty() && stack.isEmpty();
-	    }
+	private static HashMap<Integer, Integer> my_bills = new HashMap<>();
+	public static boolean lemonadeChange(int[] bills) {
+		if(bills.length == 0)
+			return true;
+	
+		my_bills.put(bills[0], 1);
+		for (int i = 1; i < bills.length; i++)
+			if(!getChange(bills[i]))
+				return false;
+			else
+				my_bills.put(bills[i], my_bills.getOrDefault(bills[i], 0) + 1);
+        return true;
+    }
+	public static boolean getChange(int bill) {
+		int change = 0;
+		HashMap<Integer, Integer> temp = new HashMap<>();
+		while (change != bill - 5)
+			if(bill - change -5 >= 20 && my_bills.getOrDefault(20, 0) > 0)
+			{
+				my_bills.put(20, my_bills.get(20) - 1);
+				change += 20;
+				temp.put(20, temp.getOrDefault(20, 0));
+			}
+			else if (bill - change -5>= 10 && my_bills.getOrDefault(10, 0) > 0)
+			{
+				my_bills.put(10, my_bills.get(10) - 1);
+				change += 10;
+				temp.put(10, temp.getOrDefault(10, 0));
+			}
+			else if (bill - change -5>= 5 && my_bills.getOrDefault(5, 0) > 0)
+			{
+				my_bills.put(5, my_bills.get(5) - 1);
+				change += 5;
+				temp.put(5, temp.getOrDefault(5, 0));
+			}
+			else
+			{
+				if (change != bill - 5)
+					for(int key : temp.keySet())
+						my_bills.put(key, my_bills.get(key) - temp.get(key));
+				return change == bill - 5;
+			}
+		return true;
 	}
 	
     public static void main(String[] args) {
-    	MyQueue obj = new MyQueue();
-    	obj.push(1);
-    	obj.push(2);
-    	obj.push(3);
-    	obj.push(4);
-    	int param_2 = obj.pop();
-    	System.out.println(param_2);
-    	int param_3 = obj.peek();
-    	System.out.println(param_3);
-    	param_2 = obj.pop();
-    	System.out.println(param_2);
-    	boolean param_4 = obj.empty();
-    	System.out.println(param_4);
+    	int[] bills = {5,5,10,10,25};
+    	System.out.println(lemonadeChange(bills));
     	
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
