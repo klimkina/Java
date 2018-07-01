a0a452ca4dc10bb67f266f3b93913ba5acd3a37f
Klimkina Liudmila <klimkina@gmail.com>
2018-07-01 15:04:23 -0700

    roman

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,29 +8,39 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static int maxA(int N) {
-		if(N <= 6)
-			return N;
-		int[] max = new int[N + 1];
-		max[0] = 0;
-		for(int i = 1; i <= 6; i++)
-			max[i] = i;
-		for(int i = 7; i <= N; i++)
-			max[i] = Math.max(max[i-1] + 1, maxA(max, i));
+	static HashMap<Character, Integer> romans = new HashMap<>();
+	
+	public static int romanToInt(String s) {
+		romans.put('I', 1);
+		romans.put('V', 5);
+		romans.put('X', 10);
+		romans.put('L', 50);
+		romans.put('C', 100);
+		romans.put('D', 500);
+		romans.put('M', 1000);
 		
-        return max[N];
-    }
-	private static int maxA(int[]max, int i)
-	{
-		int res = max[i-3] * 2;
-		for(int j = i-4; j >= i/2; j--)
-			res = Math.max(res, max[j] * (i-j-1));
+		char[] charr = s.toCharArray();
+		int res = 0;
+		int prev = 0;
+		int curr = 0;
+		for(int i = 0; i < charr.length; i++)
+		{
+			curr = romans.get(charr[i]);
+			if (prev < curr)
+			{
+				curr = curr - prev;
+				prev = 0;
+			}
+			res += prev;
+			prev = curr;
+		}
+		res += prev;
 		return res;
 	}
 	
     public static void main(String[] args) {
-    	System.out.println(maxA(3));
-    	System.out.println(maxA(9));
+    	System.out.println(romanToInt("LVIII"));
+    	System.out.println(romanToInt("MCMXCIV"));
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
     	
