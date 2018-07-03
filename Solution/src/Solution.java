97df2e8c5550addf46253d24a82a5fac4b9e09f3
Klimkina Liudmila <klimkina@gmail.com>
2018-07-02 20:59:22 -0700

    excel columns

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,37 +8,20 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static class TreeNode {
-		 int val;
-		 TreeNode left;
-		 TreeNode right;
-		 TreeNode(int x) { val = x; }
-		 }
-	private static int min = Integer.MIN_VALUE;
-	private static int min_level = 0;
-	public static int findBottomLeftValue(TreeNode root) {
-		min = root.val;
-		findBottomLeftValue(root, 0);
-	    return min;  
-    }
-		
-	public static void findBottomLeftValue(TreeNode root, int level) {
-		if(root.left == null && root.right == null)
-		{
-			if(level > min_level)
-				min = root.val;
-		}
-	   if(root.left != null)
-		   findBottomLeftValue(root.left, level + 1);
-	   if(root.right != null)
-		   findBottomLeftValue(root.left, level + 1);
+	public static String convertToTitle(int n) {
+        String res = "";
+        
+        while(n > 0)
+        {
+        	n--;
+        	res = (char)('A' + n % 26 ) + res;
+        	n = n/26;
+        }
+        return res;
     }
 	
     public static void main(String[] args) {
-    	TreeNode root = new TreeNode(1);
-    	TreeNode node = new TreeNode(1);
-    	root.right = node;
-    	System.out.println(findBottomLeftValue(root));
+    	System.out.println(convertToTitle(701));
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
     	
