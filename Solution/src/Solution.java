04b7c5a5dd98df863a2026c5b5f163853cf8b63d
Klimkina Liudmila <klimkina@gmail.com>
2018-07-04 15:19:16 -0700

    calculator

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,96 +8,95 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		int[] a, b;
-		if(nums1.length <= nums2.length)
-		{
-			a = nums1;
-			b = nums2;
-		}
-		else
-		{
-			a = nums2;
-			b = nums1;
-		}
-		int n = a.length;
-		int m = b.length;
-		int min_index = 0, max_index = n, i = 0, j = 0, median = 0;
-		int index = (n + m + 1) / 2;         
-        while (min_index <= max_index)
+	public static int calculate(String s) {
+		char[] charr = s.toCharArray();
+        Stack<Character> op_stack = new Stack<>();//(2+6* 3+5- (3*14/7+2)*5)+3
+        Stack<Integer> term_stack = new Stack<>();
+        int curr = 0;
+        while(curr < charr.length)
         {
-            i = (min_index + max_index) / 2;
-            j = index - i;
-         
-            // if i = n, it means that Elements 
-            // from a[] in the second half is an 
-            // empty set. and if j = 0, it means 
-            // that Elements from b[] in the first
-            // half is an empty set. so it is 
-            // necessary to check that, because we
-            // compare elements from these two 
-            // groups. Searching on right
-            if (i < n && j > 0 && b[j - 1] > a[i])     
-                min_index = i + 1;
-                     
-            // if i = 0, it means that Elements
-            // from a[] in the first half is an 
-            // empty set and if j = m, it means 
-            // that Elements from b[] in the second
-            // half is an empty set. so it is 
-            // necessary to check that, because 
-            // we compare elements from these two
-            // groups. searching on left
-            else if (i > 0 && j < m && b[j] < a[i - 1])     
-                max_index = i - 1;
-     
-            // we have found the desired halves.
-            else
-            {
-                // this condition happens when we 
-                // don't have any elements in the 
-                // first half from a[] so we
-                // returning the last element in 
-                // b[] from the first half.
-                if (i == 0)         
-                    median = b[j - 1];         
-                 
-                // and this condition happens when 
-                // we don't have any elements in the
-                // first half from b[] so we 
-                // returning the last element in 
-                // a[] from the first half.
-                else if (j == 0)         
-                    median = a[i - 1];         
-                else       
-                    median = Math.max(a[i - 1], b[j - 1]);         
-                break;
-            }
+        	if (charr[curr] == '+' || charr[curr] == '-' ||
+        			charr[curr] == '*' || charr[curr] == '/' || charr[curr] == '(')
+        	{
+        		op_stack.push(charr[curr++]);
+        	}
+        	else if (charr[curr]>= '0' && charr[curr] <= '9')
+        	{
+        		curr = readInt(charr, curr, term_stack);
+        		if(!op_stack.empty())
+        		{
+        			char ch = op_stack.pop();
+        			if (ch == '*' || ch == '/')
+        			{
+        				int r = term_stack.pop();
+        				int l = term_stack.pop();
+        				if (ch == '*')
+        					term_stack.push(r*l);
+        				else
+        					term_stack.push(l/r);
+        			}
+        			else
+        				op_stack.push(ch);
+        		}
+        	}
+        	else if (charr[curr] == ' ')
+        		curr++;
+        	else if (charr[curr] == ')')
+        	{
+        		calcStack(op_stack, term_stack);
+        		curr++;
+        	}
         }
-         
-        // calculating the median.
-        // If number of elements is odd 
-        // there is one middle element.
-        if ((n + m) % 2 == 1)
-            return (double)median;
-             
-        // Elements from a[] in the second 
-        // half is an empty set. 
-        if (i == n)
-            return (median+b[j]) / 2.0;
-             
-        // Elements from b[] in the second 
-        // half is an empty set.
-        if (j == m)
-            return (median + a[i]) / 2.0;
-         
-        return (median + Math.min(a[i], b[j])) / 2.0;
+        calcStack(op_stack, term_stack);
+        return term_stack.pop();
     }
 	
+	private static void calcStack(Stack<Character> op_stack, Stack<Integer> stack)
+	{
+		int res = Integer.valueOf(stack.pop());
+		boolean par_met = false;
+		if(!stack.empty())
+		{
+			char ch = op_stack.pop();
+			while(!stack.empty() && ch != '(' && !(par_met && (ch == '+' || ch == '-')))
+			{
+				int l = Integer.valueOf(stack.pop());
+				switch (ch)
+				{
+					case '+' : res += l; break;
+					case '-' : res = l - res; break;
+					case '*' : res *= l; break;
+					case '/' : res = l/res; break;
+				}
+				if (!stack.empty())
+				{
+					ch = op_stack.pop();
+					if (ch == '(' && !par_met)
+					{
+						par_met = true;
+						if (!stack.empty())
+							ch = op_stack.pop();
+					}
+				}
+			}
+			if(ch == '+' || ch == '-')
+				op_stack.push(ch);
+		}
+		stack.push(res);
+	}
+	private static int readInt(char[] charr, int pos, Stack<Integer> stack)
+	{
+		int start = pos;
+		while(pos < charr.length && charr[pos]>= '0' && charr[pos] <= '9')
+			pos++;
+		stack.push(Integer.valueOf(String.valueOf(charr, start, pos-start)));
+		return pos;
+	}
+	
     public static void main(String[] args) {
-    	int[] nums1 = {1, 2};
-    	int[] nums2 = {3, 4};
-    	System.out.println(findMedianSortedArrays(nums1, nums2));
+    	//System.out.println(calculate("1 + 1"));
+    	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
+    	System.out.println(calculate("(2+6* 3+5- (3*14/7+2)*5)+3")); // -12
     	//String res = Arrays.stream(findPair(input, n)).mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", "));
     	//System.out.println(res);
     	
