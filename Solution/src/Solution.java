4fe9b4e184178268f225f370167c77fa0a37df84
Klimkina Liudmila <klimkina@gmail.com>
2018-07-04 22:20:42 -0700

    permutations

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -9,43 +9,40 @@ import java.util.stream.Stream;
 
 public class Solution {
 	
-	public static class Interval {
-		 int start;
-		 int end;
-		 Interval() { start = 0; end = 0; }
-		 Interval(int s, int e) { start = s; end = e; }
-		 }
-	static class IntervalComparator implements Comparator{
-		public int compare(Object o1, Object o2){
-		Interval i1 = (Interval)o1;
-		Interval i2 = (Interval)o2;
-		return i1.start - i2.start;
-		}
-	}
-	public static List<Interval> merge(List<Interval> intervals) {
-		Collections.sort(intervals, new IntervalComparator());
-	    for(int i = 0;i<intervals.size()-1;i++)
-	    {
-	        int j = i+1;
-	        if(intervals.get(j).start<=intervals.get(i).end)
-	        {
-	            intervals.get(i).end = Math.max(intervals.get(j).end,intervals.get(i).end);
-	            //intervals.get(i).start = Math.min(intervals.get(j).start,intervals.get(i).start);
-	            intervals.remove(j);
-	            i--;
-	        }
-	    }
-	    return(intervals);
+	public static List<List<Integer>> permuteUnique(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i < nums.length; i++)
+			map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+		List<Integer> start = new ArrayList<>();
+		res = helper(map, start);
+		return res;
     }
 	
+	private static List<List<Integer>> helper(HashMap<Integer, Integer> map, List<Integer> start)
+	{
+		List<List<Integer>> res = new ArrayList<>();
+		if (map.values().stream().allMatch(x -> x == 0))
+			res.add(start);
+		else
+			for(int key : map.keySet())
+			{
+				if(map.get(key) > 0)
+				{
+					List<Integer> new_start = new ArrayList<Integer>(start);
+					new_start.add(key);
+					map.put(key, map.get(key) - 1);
+					res.addAll(helper(map, new_start));
+					map.put(key, map.getOrDefault(key, 0) + 1);
+				}
+			}
+		return res;
+	}
     public static void main(String[] args) {
-    	int[][] is = {{1,4},{1,4}};
-    	List<Interval> intervals = new ArrayList<>();
-    	for(int[] i : is)
-    		intervals.add(new Interval(i[0], i[1]));
-    	List<Interval> res = merge(intervals);
-    	for (Interval i : res)
-    		System.out.println(i.start + " " + i.end);
+    	int[] nums = {1,1,2};
+    	List<List<Integer>> res = permuteUnique(nums);
+    	for (List<Integer> perm : res)
+    		System.out.println(perm.stream().map(i -> i.toString()).collect(Collectors.joining(", ")));
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
     	//System.out.println(calculate("(2+6* 3+5- (3*14/7+2)*5)+3")); // -12
