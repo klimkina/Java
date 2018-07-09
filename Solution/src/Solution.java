065ea8e1729beab38149ced3eac2fee0420adcdc
Klimkina Liudmila <klimkina@gmail.com>
2018-07-08 21:48:12 -0700

    arrows and baloons

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,39 +8,47 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static class TreeNode {
-		 int val;
-		 TreeNode left;
-		 TreeNode right;
-		 TreeNode(int x) { val = x; }
-		 }
-	private static int in_pos, post_pos;
-	public static TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(inorder.length != postorder.length)
-            return null;
-        in_pos = inorder.length - 1;
-        post_pos = in_pos;
-        TreeNode root = buildTree(inorder, postorder, null);
-        return root;
-    }
-    
-    public static TreeNode buildTree(int[] inorder, int[] postorder, Integer val) {
-        if (in_pos < 0 || (val != null && inorder[in_pos] == val))
-            return null;
-        TreeNode root = new TreeNode(postorder[post_pos]); 
-        post_pos--;
-        root.right = buildTree(inorder, postorder, root.val);
-        in_pos--;
-        root.left = buildTree(inorder, postorder, val);
-        
-        return root;
+	public static class Baloon implements Comparable<Baloon>
+	{
+		int left;
+		int right;
+		public Baloon(int l, int r)
+		{
+			left = l; right = r;
+		}
+		public int compareTo(Baloon that)
+		{
+			if (this.left != that.left)
+				return this.left - that.left;
+			return this.right - that.right;
+		}
+	}
+	public static int findMinArrowShots(int[][] points) {
+		if(points.length == 0)
+			return 0;
+        Baloon[] baloons = new Baloon[points.length];
+        for (int i = 0; i < baloons.length; i++)
+        	baloons[i] = new Baloon(points[i][0], points[i][1]);
+        Arrays.sort(baloons);
+        int res = 0;
+        int last_arrow = baloons[0].left-1;
+        Queue<Integer> right_ends = new PriorityQueue<>();
+        for (int i = 0; i < baloons.length; i++)
+        {        	
+        	if(!right_ends.isEmpty() && baloons[i].left > right_ends.peek()) //add new arrow to prev baloons
+        	{
+        		res++;
+        		last_arrow = right_ends.poll();
+        		right_ends.clear();
+        	}   
+            right_ends.offer(baloons[i].right);
+        }
+        return res+1;
     }
 		
     public static void main(String[] args) {
-    	int[] inorder = {9,3,15,20,7};
-    	int[] postorder = {9,15,7,20,3};
-    	
-    	System.out.println(buildTree(inorder, postorder).val);
+    	int[][] baloons = {{10,16}, {2,8}, {1,6}, {7,12}};
+    	System.out.println(findMinArrowShots(baloons));
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
     	//System.out.println(calculate("(2+6* 3+5- (3*14/7+2)*5)+3")); // -12
