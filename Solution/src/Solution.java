4cecfe70d71f28ea05c7dce4d7b0117e1017228a
Klimkina Liudmila <klimkina@gmail.com>
2018-07-09 10:14:57 -0700

    search in rotated array

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,47 +8,56 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static class Baloon implements Comparable<Baloon>
-	{
-		int left;
-		int right;
-		public Baloon(int l, int r)
-		{
-			left = l; right = r;
-		}
-		public int compareTo(Baloon that)
-		{
-			if (this.left != that.left)
-				return this.left - that.left;
-			return this.right - that.right;
-		}
-	}
-	public static int findMinArrowShots(int[][] points) {
-		if(points.length == 0)
-			return 0;
-        Baloon[] baloons = new Baloon[points.length];
-        for (int i = 0; i < baloons.length; i++)
-        	baloons[i] = new Baloon(points[i][0], points[i][1]);
-        Arrays.sort(baloons);
-        int res = 0;
-        int last_arrow = baloons[0].left-1;
-        Queue<Integer> right_ends = new PriorityQueue<>();
-        for (int i = 0; i < baloons.length; i++)
-        {        	
-        	if(!right_ends.isEmpty() && baloons[i].left > right_ends.peek()) //add new arrow to prev baloons
-        	{
-        		res++;
-        		last_arrow = right_ends.poll();
-        		right_ends.clear();
-        	}   
-            right_ends.offer(baloons[i].right);
-        }
-        return res+1;
+	public static int search(int[] nums, int target) {
+		int pivot = findPivot (nums);
+		if (pivot < 0)
+			return (binarySearch(nums, target, 0, nums.length - 1));
+		if (nums[pivot] == target)
+			return pivot;
+		if (nums[0] <= target)
+			return (binarySearch(nums, target, 0, pivot - 1));
+        return (binarySearch(nums, target, pivot +1, nums.length - 1));
     }
+	
+	public static int binarySearch(int[] nums, int target, int start, int end) {
+		int lo = start;
+		int hi = end;
+		while (lo <= hi)
+		{
+			int m = lo + (hi-lo)/2;
+			if ( nums[m] == target)
+				return m;
+			if (nums[m] < target)
+				lo = m + 1;
+			else
+				hi = m - 1;			
+		}
+		return -1;
+	}
+	
+	private static int findPivot(int[] nums)
+	{
+		int lo = 0;
+		int hi = nums.length-1;
+		while (lo <= hi)
+		{
+			int m = lo + (hi-lo)/2;
+			if ( m < hi && nums[m + 1] < nums[m])
+				return m;
+			if (m > lo && nums[m] < nums[m-1])
+				return m;
+			if (nums[lo] >= nums[m])
+				hi = m - 1;
+			else
+				lo = m + 1;
+		}
+		return -1;
+	}
 		
     public static void main(String[] args) {
-    	int[][] baloons = {{10,16}, {2,8}, {1,6}, {7,12}};
-    	System.out.println(findMinArrowShots(baloons));
+    	int[] nums = {4,5,6,7,8,1,2};
+    	int target = 8;
+    	System.out.println(search(nums, target));
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
     	//System.out.println(calculate("(2+6* 3+5- (3*14/7+2)*5)+3")); // -12
