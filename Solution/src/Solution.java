2454f8019cca06997b567f6c0483e3e4f9f783f8
Klimkina Liudmila <klimkina@gmail.com>
2018-07-08 18:23:04 -0700

    inorder-postorder tree

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,46 +8,39 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	
-	public static class LRUCache {
-		static Map<Integer, Integer> map;
-		static int capacity;
-	    public LRUCache(int capacity) {
-	        this.capacity =  capacity;
-	        map = new LinkedHashMap<>();
-	    }
-	    
-	    public static int get(int key) {
-	        Integer val = map.get(key);
-	        if (val == null)
-	            return -1;
-	        map.remove(key);
-	        map.put(key, val);
-	        return val;
-	    }
-	    
-	    public static void put(int key, int value) {
-	        if (map.containsKey(key))
-	            map.remove(key);
-	        else if(map.size() == capacity)
-	        {
-	            int k = map.keySet().iterator().next();
-	            map.remove(k);
-	        }
-	        map.put(key, value);
-	    }
-	}
+	public static class TreeNode {
+		 int val;
+		 TreeNode left;
+		 TreeNode right;
+		 TreeNode(int x) { val = x; }
+		 }
+	private static int in_pos, post_pos;
+	public static TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder.length != postorder.length)
+            return null;
+        in_pos = inorder.length - 1;
+        post_pos = in_pos;
+        TreeNode root = buildTree(inorder, postorder, null);
+        return root;
+    }
+    
+    public static TreeNode buildTree(int[] inorder, int[] postorder, Integer val) {
+        if (in_pos < 0 || (val != null && inorder[in_pos] == val))
+            return null;
+        TreeNode root = new TreeNode(postorder[post_pos]); 
+        post_pos--;
+        root.right = buildTree(inorder, postorder, root.val);
+        in_pos--;
+        root.left = buildTree(inorder, postorder, val);
+        
+        return root;
+    }
 		
     public static void main(String[] args) {
-    	LRUCache obj = new LRUCache(2);
-    	obj.put(2,6);
-    	System.out.println(obj.get(1));
-    	obj.put(1,5);
-    	System.out.println(obj.get(1));
-    	obj.put(1,2);
-    	System.out.println(obj.get(2));
+    	int[] inorder = {9,3,15,20,7};
+    	int[] postorder = {9,15,7,20,3};
     	
-    	System.out.println(obj.get(1));
+    	System.out.println(buildTree(inorder, postorder).val);
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
     	//System.out.println(calculate("(2+6* 3+5- (3*14/7+2)*5)+3")); // -12
