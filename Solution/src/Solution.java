e042fbfc284e0b4255cac1ce98912fc5b01d530f
Klimkina Liudmila <klimkina@gmail.com>
2018-07-09 17:05:18 -0700

    zigzagLevelOrder

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,60 +8,36 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	static class RandomListNode {
-		int label;
-		RandomListNode next, random;
-		RandomListNode(int x) { this.label = x; }
-	};
-			
-	private static HashMap<RandomListNode, RandomListNode> copied;
-    public static RandomListNode copyRandomList(RandomListNode head) {
-    	copied = new HashMap<>();
-        RandomListNode node = copyNode(head);
-        RandomListNode prev = node;
-        RandomListNode res = node;
-        RandomListNode next = head;
-        while(prev != null && next != null)
-        {
-            next = next.next;
-            node = copyNode(next);
-            prev.next = node;
-            prev = node;
-        }
-        return res;
-    }
-
-    private static RandomListNode copyNode (RandomListNode head)
-    {
-        if (head == null)
-            return null;
-        if(copied.containsKey(head))
-            return copied.get(head);
-        RandomListNode node = new RandomListNode(head.label);  
-        if (copied.containsKey(head.next))
-            node.next = copied.get(head.next);
-        copied.put(head, node);
-        if (head.random != null)
-        {
-            if (copied.containsKey(head.random))
-                node.random = copied.get(head.random);
-            else
-            {
-                RandomListNode random = copyNode(head.random);
-                node.random = random;
+	public static class TreeNode {
+		    int val;
+		    TreeNode left;
+		     TreeNode right;
+		    TreeNode(int x) { val = x; }
+		 }
+		 
+	public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		List<List<Integer>> list = new ArrayList<>();
+		Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> tmp = new LinkedList<>();
+            for(int i = 0; i < size; i++){
+                if(q.peek().left != null){ q.add(q.peek().left);}
+                if(q.peek().right != null){ q.add(q.peek().right);}
+                if(list.size() % 2 == 1) 
+                    tmp.add(0, q.remove().val);
+                else 
+                    tmp.add(q.remove().val);
             }
-        }      
-        
-        return node;
+            list.add(tmp);
+        }
+        return list;
     }
-		
+	
+	
     public static void main(String[] args) {
-    	RandomListNode node1 = new RandomListNode(1);
-    	RandomListNode node2 = new RandomListNode(2);
-    	node1.next = node2;
-    	node1.random = node2;
-    	node2.random = node1;
-    	RandomListNode copy = copyRandomList(node1);
+    	
     	//System.out.println(search(nums, target));
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
