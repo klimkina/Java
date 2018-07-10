da7b5f2914105010b8fc1651c903680869e0a9a6
Klimkina Liudmila <klimkina@gmail.com>
2018-07-09 17:41:12 -0700

    sort colors

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,36 +8,32 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static class TreeNode {
-		    int val;
-		    TreeNode left;
-		     TreeNode right;
-		    TreeNode(int x) { val = x; }
-		 }
-		 
-	public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-		List<List<Integer>> list = new ArrayList<>();
-		Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            List<Integer> tmp = new LinkedList<>();
-            for(int i = 0; i < size; i++){
-                if(q.peek().left != null){ q.add(q.peek().left);}
-                if(q.peek().right != null){ q.add(q.peek().right);}
-                if(list.size() % 2 == 1) 
-                    tmp.add(0, q.remove().val);
-                else 
-                    tmp.add(q.remove().val);
-            }
-            list.add(tmp);
+	public static void sortColors(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int curr = 0;
+        while (l <= r && curr <= r)
+        {
+        	if (nums[curr] == 0)
+        	{
+        		nums[curr] = nums[l];
+        		nums[l++] = 0;
+        		curr++;
+        	} else if (nums[curr] == 2)
+        	{
+        		nums[curr] = nums[r];
+        		nums[r--] = 2;
+        	} else
+        		curr++;
         }
-        return list;
     }
 	
 	
     public static void main(String[] args) {
-    	
+    	int[] input = {2,0,2,1,1,0};
+    	sortColors(input);
+    	for(int i = 0; i < input.length; i++)
+    		System.out.print(input[i] + " ");
     	//System.out.println(search(nums, target));
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
