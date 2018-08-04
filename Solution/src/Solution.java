02f14b40d2346e02422ce9c8b2ffa951da4570f7
Klimkina Liudmila <klimkina@gmail.com>
2018-08-04 15:22:48 -0700

    merge arrays

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,25 +8,30 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static int firstUniqChar(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        char[] charr = s.toCharArray();
-        for(int i = 0; i < charr.length; i++)
-        {
-            if(map.containsKey(charr[i]))
-                map.put(charr[i], -1);
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+		int cur1 = 0;
+        int cur2 = 0;
+        int tail = m;
+        for(int i = 0; i < n + m && cur2 < n; i++)
+            if(tail > i && nums1[cur1] <= nums2[cur2])
+                cur1++;
             else
-                map.put(charr[i], i);
-        }
-        int res = -1;
-        for(int i:map.values())
-            if(res == -1 || (i >= 0 && i < res))
-                res = i;
-        return res;
-    }
+            {
+                for(int k = tail; k > cur1; k--)
+                    nums1[k] = nums1[k-1];
+                tail++;
+                nums1[cur1++] = nums2[cur2++];
+            }
+	}
 	
     public static void main(String[] args) {
-    	System.out.println(firstUniqChar("leetcolde"));
+    	int[] nums1 = {1};
+    	int m = 1;
+    	int[] nums2 = {};
+    	int n = 0;
+    	merge(nums1, m, nums2, n);
+    	for(int i = 0; i < n+m; i++)
+    		System.out.print(nums1[i] + " ");
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
     	//System.out.println(calculate("(2+6* 3+5- (3*14/7+2)*5)+3")); // -12
