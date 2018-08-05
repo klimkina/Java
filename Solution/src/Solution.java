071732d492bbc523837d2ac2f474326a6226113a
Klimkina Liudmila <klimkina@gmail.com>
2018-08-05 13:20:45 -0700

    trap water

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,39 +8,19 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(int i = 0; i < strs.length; i++)
-        {
-            String code = helper(strs[i]);
-            if (!map.containsKey(code))
-            {
-                List<String> list = new ArrayList<>();
-                map.put(code, list);
-            }
-            map.get(code).add(strs[i]);
-        }
-        List<List<String>> res = new ArrayList<>();
-        for(String key : map.keySet())
-            res.add(map.get(key));
-        return res;
-    }
-    private static String helper(String s)
-    {
-        char[] charr = s.toCharArray();
-        Arrays.sort(charr);
-        return String.valueOf(charr);
+	public static int trap(int[] height) {
+		int l = 0, r = height.length-1, level = 0, water = 0;
+	    while (l < r) {
+	        int lower = height[height[l] < height[r] ? l++ : r--];
+	        level = Math.max(level, lower);
+	        water += level - lower;
+	    }
+	    return water;
     }
 	
     public static void main(String[] args) {
-    	String[] s = {"eat", "tea", "tan", "ate", "nat", "bat", "tab"};
-    	List<List<String>> res = groupAnagrams(s);
-    	for (List<String> list : res)
-    	{
-    		for(String str : list)
-    			System.out.print(str + " ");
-    		System.out.println();
-    	}
+    	int[] height = {4,2,3};
+    	System.out.println(trap(height));
     	
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
