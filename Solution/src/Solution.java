4be428a8ecdc1b11295524e9aaca86701eef76c4
Klimkina Liudmila <klimkina@gmail.com>
2018-08-05 13:56:57 -0700

    rotate matrix

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,19 +8,55 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Solution {
-	public static int trap(int[] height) {
-		int l = 0, r = height.length-1, level = 0, water = 0;
-	    while (l < r) {
-	        int lower = height[height[l] < height[r] ? l++ : r--];
-	        level = Math.max(level, lower);
-	        water += level - lower;
-	    }
-	    return water;
+	public static void setZeroes(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return;
+        boolean isTopZero = false;
+        boolean isLeftZero = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++)
+            if (matrix[i][0] == 0)
+                isLeftZero = true;
+        
+        for (int i = 0; i < n; i++)
+            if (matrix[0][i] == 0)
+                isTopZero = true;
+        
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == 0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+        for (int i = 1; i < m; i++)
+            if (matrix[i][0] == 0)
+                for (int j = 0; j < n; j++)
+                    matrix[i][j] = 0;
+        
+        for (int j = 1; j < n; j++)
+            if (matrix[0][j] == 0)
+                for (int i = 0; i < m; i++)
+                    matrix[i][j] = 0;
+        
+        if (isTopZero)
+            for (int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        if (isLeftZero)
+            for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
     }
 	
     public static void main(String[] args) {
-    	int[] height = {4,2,3};
-    	System.out.println(trap(height));
+    	int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+    	setZeroes(matrix);
+    	for (int i = 0; i < matrix.length; i++)
+    	{
+            for (int j = 0; j < matrix[0].length; j++)
+            	System.out.print(matrix[i][j] + " ");
+            System.out.println();
+    	}
     	
     	//System.out.println(calculate("1 + 1"));
     	//System.out.println(calculate("2*(5+5*2)/3+(6/2+8)")); //21
