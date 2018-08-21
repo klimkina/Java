70112e5cd43e540a5368cac3fa1fbdfd5951957c
Klimkina Liudmila <klimkina@gmail.com>
2018-08-20 19:52:46 -0700

    commonancestor

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -11,49 +11,65 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class Solution {
-	public static boolean possibleBipartition(int N, int[][] dislikes) {
-	        int[] colors = new int[N+1];
-	        HashMap<Integer, Set<Integer>> neighbours = new HashMap<>();
-	        for (int i = 0; i <= N; i++)
-	        	neighbours.put(i, findNeighbours(i, dislikes));
-	        for (int i = 0; i <= N; i++)
-	        {
-	        	if (colors[i] == 0)
-	        	{
-	        		if (!color(i, -1, neighbours, colors))
-	        			return false;
-	        	}
-	        }
-	        
-	        return true;
-	    }
-	
-	private static boolean color (int person, int color, HashMap<Integer, Set<Integer>> neighbours, int[] colors)
-	{
-		if (colors[person] != 0 && colors[person] != color)
-			return false;
-		if (colors[person] != 0)
-            return true;
-		colors[person] = color;
-		for (int neib : neighbours.get(person))
-			if (!color (neib, -color, neighbours, colors))
-				return false;
-		return true;
-	}
-	
-	private static Set<Integer> findNeighbours(int person, int[][] dislikes)
-	{
-		Set<Integer> set = new HashSet<>();
-		for (int i = 0; i < dislikes.length; i++)
-			if (dislikes[i][0] == person || dislikes[i][1] == person)
-				set.add(dislikes[i][0] == person ? dislikes[i][1] : dislikes[i][0]);
-		return set;
-	}
-
-	    public static void main(String[] args) 
-	    {
-	    	int[][] dislikes = {{1,2},{1,3},{2,4}};
-	    	System.out.println(possibleBipartition(4, dislikes));
-	    	
-	    }
+	public static class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode(int x) { val = x; }
+		  }
+	public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		List<TreeNode> pPath = getPath(root, p);
+        List<TreeNode> qPath = getPath(root, q);
+        TreeNode curr = root;
+        TreeNode qcurr = root;
+        TreeNode res = root;
+        ListIterator<TreeNode> pIterator = pPath.listIterator();
+        ListIterator<TreeNode> qIterator = qPath.listIterator();
+        while (qIterator.hasNext() && pIterator.hasNext())
+        {
+        	res = curr;
+            curr = pIterator.next();
+            qcurr = qIterator.next();
+            if (curr != qcurr)
+            	break;
+        }
+        return res;
+    }
+    
+    private static List<TreeNode> getPath(TreeNode root, TreeNode p)
+    {
+        if (root == null)
+            return null;
+        List<TreeNode> path = null;
+        if (root == p)
+        {
+            path = new ArrayList<>();            
+        }        
+        else if (root.left != null)
+            path = getPath (root.left, p);
+        if (path == null && root.right != null)
+            path = getPath (root.right, p);
+        if (path != null)
+        	path.add(0,root);
+        return path;
+    }
+    public static void main(String[] args) 
+    {
+    	TreeNode root = new TreeNode(6);
+    	TreeNode left = new TreeNode(2);
+    	TreeNode right = new TreeNode (8);
+    	root.left = left; root.right = right;
+    	TreeNode rroot = new TreeNode(0);
+    	left.left = rroot;
+    	rroot = new TreeNode(4);
+    	left.right = rroot;
+    	TreeNode lleft = new TreeNode(7);
+    	TreeNode rright = new TreeNode (9);
+    	right.left = left; right.right = rright;
+    	lleft = new TreeNode(3);
+    	rright = new TreeNode (5);
+    	rroot.left = lleft; rroot.right = rright;
+    	System.out.println(lowestCommonAncestor(root, left,rroot).val);
+    	
+    }
 }
