8ca29335fea89d6da5df55873fddfb5600c899c1
Klimkina Liudmila <klimkina@gmail.com>
2018-08-20 22:57:23 -0700

    preinorder

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -17,59 +17,26 @@ public class Solution {
 		      TreeNode right;
 		      TreeNode(int x) { val = x; }
 		  }
-	public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		List<TreeNode> pPath = getPath(root, p);
-        List<TreeNode> qPath = getPath(root, q);
-        TreeNode curr = root;
-        TreeNode qcurr = root;
-        TreeNode res = root;
-        ListIterator<TreeNode> pIterator = pPath.listIterator();
-        ListIterator<TreeNode> qIterator = qPath.listIterator();
-        while (qIterator.hasNext() && pIterator.hasNext())
-        {
-        	res = curr;
-            curr = pIterator.next();
-            qcurr = qIterator.next();
-            if (curr != qcurr)
-            	break;
-        }
-        return res;
+	private static int idx = 0;
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder, inorder, 0, inorder.length-1);
+    }
+    public static TreeNode buildTree(int[] preorder, int[] inorder, int inStart, int inEnd) {
+        if (inStart > inEnd || inStart >= inorder.length || idx >= preorder.length)
+            return null;
+        TreeNode root = new TreeNode(preorder[idx++]);
+        int newStart = inStart;
+        while (inorder[inStart++] != root.val && inStart <= inEnd);
+        root.left = buildTree(preorder, inorder, newStart+1, inStart-1);
+        root.right = buildTree(preorder, inorder, inStart, inEnd);
+        return root;
     }
     
-    private static List<TreeNode> getPath(TreeNode root, TreeNode p)
-    {
-        if (root == null)
-            return null;
-        List<TreeNode> path = null;
-        if (root == p)
-        {
-            path = new ArrayList<>();            
-        }        
-        else if (root.left != null)
-            path = getPath (root.left, p);
-        if (path == null && root.right != null)
-            path = getPath (root.right, p);
-        if (path != null)
-        	path.add(0,root);
-        return path;
-    }
     public static void main(String[] args) 
     {
-    	TreeNode root = new TreeNode(6);
-    	TreeNode left = new TreeNode(2);
-    	TreeNode right = new TreeNode (8);
-    	root.left = left; root.right = right;
-    	TreeNode rroot = new TreeNode(0);
-    	left.left = rroot;
-    	rroot = new TreeNode(4);
-    	left.right = rroot;
-    	TreeNode lleft = new TreeNode(7);
-    	TreeNode rright = new TreeNode (9);
-    	right.left = left; right.right = rright;
-    	lleft = new TreeNode(3);
-    	rright = new TreeNode (5);
-    	rroot.left = lleft; rroot.right = rright;
-    	System.out.println(lowestCommonAncestor(root, left,rroot).val);
+    	int[] inorder = {1,2,3};
+    	int[] preorder = {3,2,1};
+    	System.out.println(buildTree(inorder, preorder).val);
     	
     }
 }
