bebb4cc13007d05ae3ad48c5443ba4087c63fbf6
Klimkina Liudmila <klimkina@gmail.com>
2018-08-27 20:02:02 -0700

    freq stack

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -10,33 +10,84 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-public class Solution {
-	public static class TreeNode {
-		      int val;
-		      TreeNode left;
-		      TreeNode right;
-		      TreeNode(int x) { val = x; }
-		  }
-	private static int idx = 0;
-    public static TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTree(preorder, inorder, 0, inorder.length-1);
+class Solution {
+    public static class ListElem
+    {
+        public int val;
+        public ListElem next;
+        public ListElem prev;
+        public ListElem(int val)
+        {
+            this.val = val;
+        }
+        public ListElem insert(int val)
+        {
+            ListElem elem = new ListElem (val);
+            ListElem nextnext = this.next;
+            this.next = elem;
+            elem.next = nextnext;
+            elem.prev = this;
+            if (nextnext != null)
+                nextnext.prev = elem;
+            return elem;
+        }
     }
-    public static TreeNode buildTree(int[] preorder, int[] inorder, int inStart, int inEnd) {
-        if (inStart > inEnd || inStart >= inorder.length || idx >= preorder.length)
-            return null;
-        TreeNode root = new TreeNode(preorder[idx++]);
-        int newStart = inStart;
-        while (inorder[inStart++] != root.val && inStart <= inEnd);
-        root.left = buildTree(preorder, inorder, newStart+1, inStart-1);
-        root.right = buildTree(preorder, inorder, inStart, inEnd);
-        return root;
+    private TreeMap<Integer, ListElem> freq2x = new TreeMap<>();
+    private HashMap<Integer, Integer> x2freq = new HashMap<>();
+    ListElem head = null;
+    ListElem tail = null;
+    public Solution() {
+        
     }
     
+    public void push(int x) {
+        int freq = x2freq.getOrDefault(x, 0) + 1;
+        x2freq.put(x, freq);
+        if (head == null)
+        {
+            head = new ListElem(x);
+            tail = head;
+            freq2x.put(freq, head);
+        }
+        else
+        {
+            ListElem elem = freq2x.floorEntry(freq).getValue();
+            freq2x.put(freq, elem.insert(x));
+            if (elem == tail)
+                tail = elem.next;
+        }
+    }
+    
+    public int pop() {
+        int res = tail.val;        
+        if (freq2x.get(x2freq.get(res)) == tail)
+        	freq2x.put(x2freq.get(res), tail.prev);
+    	x2freq.put(res, x2freq.get(res) - 1);
+        tail = tail.prev;
+        return res;
+    }
+
     public static void main(String[] args) 
     {
-    	int[] inorder = {1,2,3};
-    	int[] preorder = {3,2,1};
-    	System.out.println(buildTree(inorder, preorder).val);
-    	
+    	Solution obj = new Solution();
+    	obj.push(2);
+    	obj.push(2);
+    	obj.push(2);
+    	obj.push(2);
+    	obj.push(2);
+    	obj.push(2);
+    	obj.push(3);
+    	obj.push(3);
+    	obj.push(3);
+    	obj.push(4);
+    	obj.push(4);
+    	System.out.println(obj.pop());
+    	System.out.println(obj.pop());
+    	obj.push(1);
+    	System.out.println(obj.pop());
+    	obj.push(1);
+    	System.out.println(obj.pop());
+    	obj.push(4);
+    	System.out.println(obj.pop());
     }
 }
