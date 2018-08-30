6d2600ee64d7aeeab1360a5d3f415579f096c277
Klimkina Liudmila <klimkina@gmail.com>
2018-08-29 20:59:38 -0700

    matrix search

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -11,24 +11,30 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Solution {
-	public static boolean isMatch(String s, String p) {
-        int n = s.length();
-        int m = p.length();
-        boolean[][] memo = new boolean[n+1][m+1];
-        memo[0][0] = true;
-        for (int i = 0; i < m; i++)
-            memo[0][i+1] = (p.charAt(i) == '*') && memo[0][i];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
-                    memo[i+1][j+1] = memo[i][j];
-                else if (p.charAt(j) == '*')
-                    memo[i+1][j+1] = memo[i+1][j] || memo[i][j+1];
-        return memo[n][m];
+	public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0; int h = m * n -1;
+        while (l < h)
+        {
+            int mid = l + (h-l)/2;
+            if (matrix[mid/n][mid%n] == target)
+                return true;
+            if (matrix[mid/n][mid%n] > target)
+                h = mid;
+            else 
+                l = mid +1;
+        }
+        if (matrix[l/n][l%n] == target)
+            return true;
+        return false;
     }
     public static void main(String[] args) 
     {
-    	
-    	System.out.println(isMatch("", "?"));
+    	int[][] matrix = {{1}
+    					};
+    	System.out.println(searchMatrix(matrix, 0));;
     }
 }
