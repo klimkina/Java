ecbece6715c353fe57df2428cbd2d5de7e8fbf4a
Klimkina Liudmila <klimkina@gmail.com>
2018-09-08 10:04:24 -0700

    median of sorted arrays

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -11,30 +11,54 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Solution {
-	public static boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-            return false;
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int l = 0; int h = m * n -1;
-        while (l < h)
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums2 == null || (nums1 != null && nums1.length  > nums2.length))
+        	return findMedianSortedArrays(nums2, nums1);
+        int idx1 = 0; 
+        int idx2 = 0;
+        int m = nums1 == null ? 0 : nums1.length;
+        int n = nums2 == null ? 0 : nums2.length;
+        int l = 0;
+        int h = m;
+        while (l <= h)
         {
-            int mid = l + (h-l)/2;
-            if (matrix[mid/n][mid%n] == target)
-                return true;
-            if (matrix[mid/n][mid%n] > target)
-                h = mid;
-            else 
-                l = mid +1;
+        	idx1 = l + (h-l)/2;
+        	idx2 = (n + m + 1)/2 - idx1;
+        	if (idx1 > 0 && idx2 < n && nums1[idx1-1] > nums2[idx2])
+    			h = idx1-1;
+        	else if (idx2 > 0 && idx1 < m && nums2[idx2-1] > nums1[idx1])
+    			l = idx1 + 1;  
+        	else
+        		break;
         }
-        if (matrix[l/n][l%n] == target)
-            return true;
-        return false;
+        return calcMedian(nums1, nums2, idx1, idx2);
+    }
+    
+    private static double calcMedian (int[] nums1, int[] nums2, int idx1, int idx2)
+    {
+    	double median = 0;
+    	if (idx1 == 0)
+    		median = nums2[idx2-1];
+    	else if (idx2 == 0)
+    		median = nums1[idx1-1];
+    	else
+    		median = Math.max(nums1[idx1-1], nums2[idx2-1]);
+    	if ((nums1.length + nums2.length) % 2 == 0)
+    	{
+    		if (idx1 == nums1.length)
+    			median += nums2[idx2];
+    		else if (idx2 == nums2.length)
+    			median += nums1[idx1];
+    		else
+    			median += Math.min(nums1[idx1], nums2[idx2]);
+    		median /= 2;
+    	}
+    	return median;
     }
     public static void main(String[] args) 
     {
-    	int[][] matrix = {{1}
-    					};
-    	System.out.println(searchMatrix(matrix, 0));;
+    	int[] nums1 = {1, 2, 3,5};
+    	int[] nums2 = {4, 5, 6};
+    	System.out.println(findMedianSortedArrays(nums1, nums2));;
     }
 }
