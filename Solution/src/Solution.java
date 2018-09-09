33ae15589680b14507d10267e850751a57d70a75
Klimkina Liudmila <klimkina@gmail.com>
2018-09-09 12:31:30 -0700

    maxbuy

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -11,54 +11,24 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Solution {
-	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums2 == null || (nums1 != null && nums1.length  > nums2.length))
-        	return findMedianSortedArrays(nums2, nums1);
-        int idx1 = 0; 
-        int idx2 = 0;
-        int m = nums1 == null ? 0 : nums1.length;
-        int n = nums2 == null ? 0 : nums2.length;
-        int l = 0;
-        int h = m;
-        while (l <= h)
+	public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0)
+            return 0;
+        int n = prices.length;        
+        int[] buys = new int[n];
+        int[] sells = new int[n];
+        buys[0] = -prices[0];
+        sells[0] = 0;
+        for (int i = 1; i < n; i++)
         {
-        	idx1 = l + (h-l)/2;
-        	idx2 = (n + m + 1)/2 - idx1;
-        	if (idx1 > 0 && idx2 < n && nums1[idx1-1] > nums2[idx2])
-    			h = idx1-1;
-        	else if (idx2 > 0 && idx1 < m && nums2[idx2-1] > nums1[idx1])
-    			l = idx1 + 1;  
-        	else
-        		break;
+            buys[i] = Math.max(buys[i-1], sells[i-1] - prices[i]);
+            sells[i] = Math.max(buys[i-1] + prices[i], sells[i-1]);
         }
-        return calcMedian(nums1, nums2, idx1, idx2);
-    }
-    
-    private static double calcMedian (int[] nums1, int[] nums2, int idx1, int idx2)
-    {
-    	double median = 0;
-    	if (idx1 == 0)
-    		median = nums2[idx2-1];
-    	else if (idx2 == 0)
-    		median = nums1[idx1-1];
-    	else
-    		median = Math.max(nums1[idx1-1], nums2[idx2-1]);
-    	if ((nums1.length + nums2.length) % 2 == 0)
-    	{
-    		if (idx1 == nums1.length)
-    			median += nums2[idx2];
-    		else if (idx2 == nums2.length)
-    			median += nums1[idx1];
-    		else
-    			median += Math.min(nums1[idx1], nums2[idx2]);
-    		median /= 2;
-    	}
-    	return median;
+        return sells[n-1];
     }
     public static void main(String[] args) 
     {
-    	int[] nums1 = {1, 2, 3,5};
-    	int[] nums2 = {4, 5, 6};
-    	System.out.println(findMedianSortedArrays(nums1, nums2));;
+    	int[] prices = {7,1,5,3,6,4};
+    	System.out.println(maxProfit(prices));
     }
 }
