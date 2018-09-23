2e8976b47d8d038c23aa0ee211b3f58cf98dbfd0
Klimkina Liudmila <klimkina@gmail.com>
2018-09-22 22:05:27 -0700

    elections

--- Solution/src/TopVotedCandidate.java
+++ Solution/src/TopVotedCandidate.java
@@ -1,22 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class TopVotedCandidate {
 	private TreeMap<Integer, Integer> tree = new TreeMap<>();
-	public TopVotedCandidate(int[] persons, int[] times) {
-        int[] winners = new int[persons.length];
+    private Map<Integer, Integer> cache = new HashMap<>();
+
+    public TopVotedCandidate(int[] persons, int[] times) {
+        Map<Integer, Integer> votes = new HashMap<>();
         int max = -1;
         for (int i = 0; i < times.length; i++)
         {
-        	winners[persons[i]]++;
-        	if (max == -1 || winners[persons[i]] >= winners[max])
+        	votes.put(persons[i], votes.getOrDefault(persons[i], 0) + 1);
+            if (max == -1 || votes.get(persons[i]) >= votes.get(max))
         		max = persons[i];
         	tree.put(times[i], max);
+            cache.put(times[i], max);
         }
     }
     
     public int q(int t) {
-        return tree.get(tree.floorKey(t));
+        if (!cache.containsKey(t)) {
+            int result = tree.floorEntry(t).getValue();
+            cache.put(t, result);
+        }
+        return cache.get(t);
     }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] persons = {0,1,1,0,0,1,0};
