72abc8639d10918638179adb439a38ccda1ac7b4
Klimkina Liudmila <klimkina@gmail.com>
2019-01-12 19:02:29 -0800

    k closest points

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -11,39 +11,29 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Solution {
-	public static int largestPerimeter(int[] A) {
-        int n = A.length;
-        Arrays.sort(A);
-        if (n < 3)
-            return 0;
-        int a = A[n-1];
-        int b = A[n-2];
-        int c = A[n-3];
-        if (check(a,b,c))
-            return (a+b+c);
-        int curr = n-4;
-        while (curr >= 0)
+	public static int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, new Comparator<int[]>() {
+    	    public int compare(int[] p1, int[] p2) {
+    	    	Integer sqDist1 = p1[0]*p1[0] + p1[1]*p1[1];
+    	    	Integer sqDist2 = p2[0]*p2[0] + p2[1]*p2[1];
+    	    	
+    	        return sqDist1 - sqDist2;
+    	    }
+    	});
+        int[][] res = new int[K][2];
+        for(int i = 0; i < K; i++)
         {
-            int d = A[curr];
-            int res = 0;
-            if (check(a,b,d))
-                res = a+b+d;
-            if (check(a,d,c) && a+d+c > res)
-                res = a+d+c;
-            if (check(d,b,c) && b+d+c > res)
-                res = b+d+c;
-            curr--;
-            a = b; b = c; c = d;
+            res[i][0] = points[i][0];
+            res[i][1] = points[i][1];
         }
-        return 0;
-    }
-    private static boolean check(int a, int b, int c) {
-        return (a < b + c) && (b < a + c) && (c < a + b);
+        return res;
     }
 	
     public static void main(String[] args) 
     {
-    	int[] A = {3, 6,2,3};
-    	System.out.print(largestPerimeter(A));
+    	int[][] A = {{3,3},{5,-1},{-2,4}};
+    	int[][] res = kClosest(A, 2);
+    	for(int i = 0; i < res.length; i++)
+    		System.out.print(res[i][0] + " " + res[i][1] + "\n");
     }
 }
