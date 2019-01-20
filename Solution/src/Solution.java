adc27caf25d1dd8ab545bcd988b9c3627cab9861
Klimkina Liudmila <klimkina@gmail.com>
2019-01-19 19:11:56 -0800

    sorted squares

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,38 +1,16 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
-	public static int subarraysDivByK(int[] A, int K) {
-        
-		int count = 0;
-        int[] group = new int[K];
-        int sum = 0;
-        for(int i = 0; i < A.length; i++)
-        {
-            sum += A[i];
-            int rem = sum % K;
-            if (rem < 0)
-                rem += K;
-            group[rem]++;
-        }
-        for (int i = 0; i < K; i++)
-            if (group[i] > 0)
-                count += group[i] * (group[i] - 1)/2;
-        return count + group[0];
+	public static int[] sortedSquares(int[] A) {
+        List<Integer> integers = Arrays.stream(A).boxed().collect(Collectors.toList());
+		int[] res = integers.stream().map(x -> (x*x)).mapToInt(i -> i).toArray();
+        Arrays.sort(res);
+        return res;
     }
 	
-    public static void main(String[] args) 
-    {
-    	int[] A = {4,5,0,-2,-3,1};
-    	System.out.print(subarraysDivByK(A, 5));
-    }
+	public static void main(final String[] args) {
+		int numCrates = 3;
+	}
 }
