364f936660860c1ce9b2c3fdac44696bdb51a038
Klimkina Liudmila <klimkina@gmail.com>
2019-02-09 19:04:10 -0800

    sum in list form

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,85 +1,54 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
 class Solution {
 	
-	// METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
-    static List<List<Integer>> optimalUtilization(int maxTravelDist, 
-                                    List<List<Integer>> forwardRouteList,
-                                    List<List<Integer>> returnRouteList)
-	{
-        // WRITE YOUR CODE HERE
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        Collections.sort(forwardRouteList, new Comparator<List<Integer>>() {
-	                public int compare(List<Integer> f1, List<Integer> f2) {
-	                    return f1.get(1).compareTo(f2.get(1));
-	                }	              
-	            });
-	    Collections.sort(returnRouteList, new Comparator<List<Integer>>() {
-	                public int compare(List<Integer> f1, List<Integer> f2) {
-	                    return f1.get(1).compareTo(f2.get(1));
-	                }	              
-	            });
-	    int max = -1;
-	    for(int i = forwardRouteList.size() - 1; i >=0; i--)
-	        for(int j = returnRouteList.size() - 1; j >=0; j--) {
-	            int dist = forwardRouteList.get(i).get(1) + returnRouteList.get(j).get(1);
-	            if(dist <= maxTravelDist) {
-	                if(dist < max)
-	                    break;
-	                if (dist > max) {
-	                    res.clear();
-	                    max = dist;
-	                }
-	                List<Integer> pair = new ArrayList<>();
-	                pair.add(forwardRouteList.get(i).get(0));
-	                pair.add(returnRouteList.get(j).get(0));
-	                res.add(pair);
-	            }
-	        }
-	   return res;
+	public static List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> k_list = new ArrayList<>();
+        while (K > 0) {
+            int num = K%10;
+            k_list.add(0, num);
+            K /= 10;
+        }
+        int rem = 0;
+        List<Integer> res = new ArrayList<>();
+        int i = k_list.size() - 1;
+        int j = A.length - 1;
+        for(; j >= 0 && (i >= 0 || rem > 0); i--, j--)
+        {
+            int sum = A[j] + (i >= 0 ? k_list.get(i) : 0) + rem;
+            if(sum > 9) {
+                rem = sum/10;
+                sum = sum %10;
+            }
+            else
+                rem = 0;
+            res.add(0, sum);
+        }
+        
+        while (j >= 0)
+        	res.add(0, A[j--]);
+        while (i >= 0)
+        {
+        	int sum = k_list.get(i--) + rem;
+        	if(sum > 9) {
+                rem = sum/10;
+                sum = sum %10;
+            }
+            else
+                rem = 0;
+        	res.add(0, sum);
+        }
+        if (rem > 0)
+        	res.add(0, rem);
+        return res;
     }
 	
 	public static void main(final String[] args) {
-		List<List<Integer>> forw = new ArrayList<List<Integer>>();
-		List<Integer> pair = new ArrayList<>();
-		pair.add(1);
-		pair.add(3000);
-		forw.add(pair);
-		pair = new ArrayList<>();
-		pair.add(2);
-		pair.add(5000);
-		forw.add(pair);
-		pair = new ArrayList<>();
-		pair.add(3);
-		pair.add(7000);
-		forw.add(pair);
-		pair = new ArrayList<>();
-		pair.add(4);
-		pair.add(10000);
-		forw.add(pair);
-		List<List<Integer>> ret = new ArrayList<List<Integer>>();
-		pair = new ArrayList<>();
-		pair.add(1);
-		pair.add(2000);
-		ret.add(pair);
-		pair = new ArrayList<>();
-		pair.add(2);
-		pair.add(3000);
-		ret.add(pair);
-		pair = new ArrayList<>();
-		pair.add(3);
-		pair.add(4000);
-		ret.add(pair);
-		pair = new ArrayList<>();
-		pair.add(4);
-		pair.add(5000);
-		ret.add(pair);
-		int max = 10000;
-		List<List<Integer>> res = optimalUtilization(max, forw, ret);
-		for(int i = 0 ; i < res.size(); i++)
-			System.out.println(res.get(i).get(0) + " " + res.get(i).get(1));
+		int[] A = {7};
+		System.out.println(addToArrayForm(A, 993));
 	}
 }
