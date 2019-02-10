e1e3427645ec06177a45d9be0a27a966f617ee46
Klimkina Liudmila <klimkina@gmail.com>
2019-02-09 17:26:10 -0800

    locations distance)

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,49 +1,46 @@
 import java.util.List;
 import java.util.Stack;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 class Solution {
-	static String str1 = "aa";
-	static String str2 = "a";
-	static String str3 = "b";
-	static String str4 = "bb";
-	public static String strWithout3a3b(int A, int B) {
-        if (B > A)
-        {
-        	str1 = "bb";
-        	str2 = "b";
-        	str3 = "a";
-        	str4 = "aa";
-            return strWithout3a3b(B, A);
-        }
-        int an = A/2;
-        int bn = B - an;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < an; i++) 
-        {
-            sb.append(str1);
-            if (bn-- > 0)
-            {
-            	if(B > 0)
-            		sb.append(str4);
-            	B -= 2;
-            }
-            else if (B > 0)
-            {
-            	B --;
-            	sb.append(str3);
-            }
-        }
-        
-        if (A % 2 > 0)
-            sb.append(str2);
-        if (B > 0)
-        	sb.append(str3);
-        return sb.toString();
-    }
+	
+	    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+	    List<List<Integer>> ClosestXdestinations(int numDestinations, 
+	                                             List<List<Integer>> allLocations,
+	                                             int numDeliveries)
+		{
+	        // WRITE YOUR CODE HERE
+	        if(numDeliveries >= numDestinations)
+	            return allLocations;
+	        Collections.sort(allLocations, new Comparator<List<Integer>>() {
+	                public int compare(List<Integer> loc1, List<Integer> loc2) {
+	                    Integer sqDist1 = sqDist(loc1);
+	                    Integer sqDist2 = sqDist(loc2);
+	                    return sqDist1.compareTo(sqDist2);
+	                }	              
+	            });
+	        List<List<Integer>> res = new ArrayList<List<Integer>>();
+	        for(int i = 0; i < Math.min(numDeliveries, allLocations.size()); i++)
+	            res.add(allLocations.get(i));
+	        return res;
+	    }
+	    // METHOD SIGNATURE ENDS
+	    private Integer sqDist(List<Integer> loc)
+	    {
+	        if(loc.isEmpty() || loc.size() != 2)
+	            throw new IllegalArgumentException("Bad location!");
+	        Integer x = loc.get(0);
+	        Integer y = loc.get(1);
+	        return x*x + y*y;    
+	    }
+	}
 	
 	public static void main(final String[] args) {
-		System.out.println(strWithout3a3b(4,2));
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		
 	}
 }
