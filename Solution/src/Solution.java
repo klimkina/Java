8d3fabc68c2e65fd102d8fc885380e5666b5c6be
Klimkina Liudmila <klimkina@gmail.com>
2019-03-03 09:15:16 -0800

    Max Consecutive Ones III

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -13,43 +13,25 @@ import java.util.Iterator;
 
 class Solution {
 	
-	public static List<String> commonChars(String[] A) {
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        List<String> res = new ArrayList<>();
-        if (A.length == 0)
-            return res;
-        for(char alphabet = 'a'; alphabet <='z'; alphabet++)
-            map1.put(alphabet, Integer.MAX_VALUE);
-        for (int i = 0; i < A.length; i++) {
-            char[] chars = A[i].toCharArray();
-            for (char ch : chars)
-                map2.put(ch, map2.getOrDefault(ch, 0) + 1);
-            Iterator<Map.Entry<Character, Integer>> iter = map1.entrySet().iterator();
-            while (iter.hasNext()) {
-                Map.Entry<Character,Integer> entry = iter.next();
-                int min = Math.min(entry.getValue(), map2.getOrDefault(entry.getKey(), 0));
-                if(min == 0){
-                    iter.remove();
-                }
-                else
-                    map1.put(entry.getKey(), min);
+	public static int longestOnes(int[] A, int K) {
+		int start = 0;
+        int max = 0;
+        if (A.length > 0) {
+            for (int i = 0; i < A.length; i++) {
+                if (A[i] == 0)
+                    K--;
+                
+                while (K < 0)
+                    if (A[start++] == 0)
+                        K++;
+                max = Math.max(max, i - start + 1); 
             }
-            map2.clear();
         }
-        Iterator<Map.Entry<Character, Integer>> iter2 = map1.entrySet().iterator();
-        while (iter2.hasNext()) {
-            Map.Entry<Character, Integer> entry = iter2.next();
-            for(int k = 0; k < entry.getValue(); k++)
-                res.add(entry.getKey().toString());
-        }
-        return res;
+        return max;
     }
 	
 	public static void main(final String[] args) {
-		String[] s = {"bella","label","roller"};
-		List<String> res = commonChars(s);
-		for (String str : res)
-			System.out.println(str);
+		int[] A = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+		System.out.println(longestOnes(A, 3));
 	}
 }
