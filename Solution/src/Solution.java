224c2347e06685d9cea37138f60d65f937173b37
Klimkina Liudmila <klimkina@gmail.com>
2019-03-02 19:09:18 -0800

    common chars

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -13,15 +13,43 @@ import java.util.Iterator;
 
 class Solution {
 	
-	public static boolean isValid(String S) {
-        if (S.isEmpty() || S.equals("abc"))
-            return true;
-        if(S.indexOf("abc") < 0)
-            return false;
-        return isValid(S.replace("abc", ""));
+	public static List<String> commonChars(String[] A) {
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        List<String> res = new ArrayList<>();
+        if (A.length == 0)
+            return res;
+        for(char alphabet = 'a'; alphabet <='z'; alphabet++)
+            map1.put(alphabet, Integer.MAX_VALUE);
+        for (int i = 0; i < A.length; i++) {
+            char[] chars = A[i].toCharArray();
+            for (char ch : chars)
+                map2.put(ch, map2.getOrDefault(ch, 0) + 1);
+            Iterator<Map.Entry<Character, Integer>> iter = map1.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry<Character,Integer> entry = iter.next();
+                int min = Math.min(entry.getValue(), map2.getOrDefault(entry.getKey(), 0));
+                if(min == 0){
+                    iter.remove();
+                }
+                else
+                    map1.put(entry.getKey(), min);
+            }
+            map2.clear();
+        }
+        Iterator<Map.Entry<Character, Integer>> iter2 = map1.entrySet().iterator();
+        while (iter2.hasNext()) {
+            Map.Entry<Character, Integer> entry = iter2.next();
+            for(int k = 0; k < entry.getValue(); k++)
+                res.add(entry.getKey().toString());
+        }
+        return res;
     }
 	
 	public static void main(final String[] args) {
-		System.out.println(isValid("cabcabc"));
+		String[] s = {"bella","label","roller"};
+		List<String> res = commonChars(s);
+		for (String str : res)
+			System.out.println(str);
 	}
 }
