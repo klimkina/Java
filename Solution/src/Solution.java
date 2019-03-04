e909fad166701b7d4a39d28fc14a12f45f87f366
Klimkina Liudmila <klimkina@gmail.com>
2019-03-03 18:58:32 -0800

    burst baloons

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -13,32 +13,38 @@ import java.util.Iterator;
 
 class Solution {
 	
-	public static int characterReplacement(String s, int k) {
-        if (s.length() < k)
-            return s.length();
-        char[] chars = s.toCharArray();
-        int max = 0;
-        int K = k;
-        for(Character alpha = 'A'; alpha <= 'Z'; alpha++)
-        {
-            int start = 0;
-            k = K;
-            for(int i = 0; i < chars.length; i++)
-            {
-                if (chars[i] != alpha)
-                    k--;
-                while (k < 0)
-                {
-                    if (chars[start++] != alpha)
-                        k++;
-                }
-                max = Math.max(max, i - start + 1);
+	public static int maxCoins(int[] nums) {
+        int n = nums.length;
+        if (n == 0)
+            return 0;
+        int[][] memo = new int[n][n];
+        for (int len = 1; len <= n; len++)
+            for (int start = 0; start <= n-len; start++) {
+            	int finish = start + len -1;
+            	int left = 1;
+            	int right = 1;
+            	if (start > 0)
+            		left = nums[start-1];
+            	if (finish < n-1)
+            		right = nums[finish +1];
+            	
+            	for(int k = start; k <= finish; k++)
+            	{
+            		int before = 0;
+            		int after = 0;
+            		if (k > start)
+            			before = memo[start][k-1];
+            		if (k < finish)
+            			after = memo[k+1][finish];
+            		memo[start][finish] = Math.max(memo[start][finish], before + after + nums[k] * left * right);
+            	}
             }
-        }
-        return max;
-    }
+                
+        return memo[0][n-1];
+    }    
 	
 	public static void main(final String[] args) {
-		System.out.println(characterReplacement("ABBB", 2));
+		int[] nums = {3,1,5,8};
+		System.out.println(maxCoins(nums));
 	}
 }
