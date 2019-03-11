f6fc1a60e4945c9d5347387f50696196b677a9dc
Klimkina Liudmila <klimkina@gmail.com>
2019-03-10 20:29:44 -0700

    missing number

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -7,46 +7,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 class Solution {
 	
-	public static int[][] imageSmoother(int[][] M) {
-        int[][] res = new int[M.length][M[0].length];
-        for(int i = 0; i < M.length; i++)
-            for(int j = 0; j < M[0].length; j++)
-                res[i][j] = calc(M, i, j);
-        return res;
-        
-    }
-    private static int calc(int[][] M, int i, int j)
-    {
-        int[] dx = {-1,0,1};
-        int[] dy = {-1,0,1};
-        int sum = 0;
-        int num = 0;
-        for(int x = 0; x < 3; x++)
-            for(int y = 0; y < 3; y++)
-            {
-                int new_x = i + dx[x];
-                int new_y = j + dy[y];
-                if (inBound(M, new_x, new_y))
-                {
-                    sum += M[new_x][new_y];
-                    num++;
-                }
-            }
-        return sum/(num);
-    }
-    
-    private static boolean inBound(int[][]M, int i, int j)
-    {
-        return i >= 0 && j >= 0 && i < M.length && j < M[0].length;
+	public static int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0;
+        while(start < nums.length && nums[start] <=0)
+            start++;
+        if (nums.length == 0 || start >= nums.length || nums[start] > 1)
+            return 1;
+        start++;
+        while (start < nums.length) {
+            if (nums[start] - nums[start-1] > 1)
+                return nums[start-1] + 1;
+            start++;
+        }
+        return nums[start-1] + 1;
     }
 	
 	public static void main(final String[] args) {
-		int[][] M = {{2,3,4},{5,6,7},{8,9,10},{11,12,13},{14,15,16}};
-		int[][] N = imageSmoother(M);
-		for(int i = 0; i < M.length; i++) {
-            for(int j = 0; j < M[0].length; j++)
-            	System.out.print(N[i][j]);
-            System.out.println();
-		}
+		int[] M = {1,1,3};
+		System.out.print(firstMissingPositive(M));
 	}
 }
