f57a8732917fdf7ea3c6e731062afae1e8d5a9de
Klimkina Liudmila <klimkina@gmail.com>
2019-03-10 20:19:16 -0700

    grayscale

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -7,60 +7,46 @@ import java.util.ArrayList;
 import java.util.Arrays;
 class Solution {
 	
-	public static boolean isNumber(String s) {
-        char[] charr = s.trim().toCharArray();
-        int n = charr.length;
-        if (n == 0)
-            return false;
-        int idx = 0;
-        if (charr[idx] == '+' || charr[idx] == '-')
-            idx++;
-        boolean isDot = false;
-        boolean isDouble = false;
-        while (idx < n && charr[idx] != 'e') // valid double to the left
-        {
-            if (charr[idx] == '.')
+	public static int[][] imageSmoother(int[][] M) {
+        int[][] res = new int[M.length][M[0].length];
+        for(int i = 0; i < M.length; i++)
+            for(int j = 0; j < M[0].length; j++)
+                res[i][j] = calc(M, i, j);
+        return res;
+        
+    }
+    private static int calc(int[][] M, int i, int j)
+    {
+        int[] dx = {-1,0,1};
+        int[] dy = {-1,0,1};
+        int sum = 0;
+        int num = 0;
+        for(int x = 0; x < 3; x++)
+            for(int y = 0; y < 3; y++)
             {
-                if (isDot)
-                    return false;
-                isDot = true;
-                if (!isDouble && (idx < n-1 && !isNumber(charr[idx+1])))
-                    return false;
+                int new_x = i + dx[x];
+                int new_y = j + dy[y];
+                if (inBound(M, new_x, new_y))
+                {
+                    sum += M[new_x][new_y];
+                    num++;
+                }
             }
-            else if (isNumber(charr[idx]))
-                isDouble = true;
-            else
-                return false;
-            idx++;
-        }
-        boolean isE = true;
-        if (idx < n && charr[idx] == 'e' && isDouble)
-        {
-            if (!isDouble || idx == n-1)
-                return false;
-            if (charr[idx+1] == '-' || charr[idx+1] == '+')
-                idx++;
-            idx++;
-            isE = false;
-            while (idx < n)
-            {
-                if (isNumber(charr[idx]))
-                    isE = true;
-                else
-                    return false;
-                idx++;
-            }
-        }
-        return isDouble && isE;
+        return sum/(num);
     }
     
-    private static boolean isNumber(char ch)
+    private static boolean inBound(int[][]M, int i, int j)
     {
-        return ch >= '0' && ch <= '9';
+        return i >= 0 && j >= 0 && i < M.length && j < M[0].length;
     }
 	
 	public static void main(final String[] args) {
-		System.out.println(isNumber("4e3."));
-		System.out.println(isNumber("+90e3.5"));
+		int[][] M = {{2,3,4},{5,6,7},{8,9,10},{11,12,13},{14,15,16}};
+		int[][] N = imageSmoother(M);
+		for(int i = 0; i < M.length; i++) {
+            for(int j = 0; j < M[0].length; j++)
+            	System.out.print(N[i][j]);
+            System.out.println();
+		}
 	}
 }
