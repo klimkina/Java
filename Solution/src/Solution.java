7b60512c8b3d9a884ddca2d2e642b4d4254270c0
Klimkina Liudmila <klimkina@gmail.com>
2019-03-17 22:51:17 -0700

    strangely compartible

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -8,38 +8,56 @@ import java.util.Arrays;
 import java.util.HashMap;
 class Solution {
 	
-	public static int visuallyBalancedSections(List<Integer> colors) {
-	    // Write your code here
-	        long[] cols = new long[colors.size()];
-	        for (int i = 0; i < cols.length; i++)
-	            cols[i] = 1L << colors.get(i); // code colors as powers of 2
-	        long res = 1L;
-	        long[] prefix = new long[cols.length];
-	        prefix[0] = cols[0];
-	        for (int i = 1; i < cols.length; i++)
-	        {
-	            prefix[i] = prefix[i-1] ^ cols[i]; // calc prefixes for each index
-	            if (isBalanced(prefix[i]))
-	            	res++;
-	        }
-	        
-	        for (int i = 0; i < cols.length - 1; i++) // calc xor for [L..R] by xor'ing prefixes for L and R
-	        	for (int j = i+1; j < cols.length; j++)
-	        		if (isBalanced(prefix[i]^prefix[j]))
-		            	res++;
-	        return (int)res;
-	    }
-
-	private static boolean isBalanced(long l)
-	{
-		for (long c = 1L; c <= (1L << 50); c = c << 1) // check if only one '1'
-            if ((l ^ c) == 0L)
-                return true;
-		return (l == 0L);
-	}
+	private static long[] pow;
+    private static HashMap<Long, Long> map = new HashMap<>();
+    private static HashMap<Long, Long> same = new HashMap<>();
+    
+    public static int strangelyCompatible(List<String> students) {
+    // Write your code here
+        int m = students.get(0).length();
+        pow = new long[m+1];
+        pow[0] = 1;
+        for (int i = 1; i <= m; i++)
+            pow[i] = pow[i-1] *31;
+        for (String s: students)
+            get_hash(s);
+        
+        int res = 0;
+        for (long l : map.values())
+            res += l*(l-1)/2L;
+        for (long l : same.values())
+            res -= (l*(l-1)/2L) * m;
+        return res;
+    }
+    private static void get_hash(String s)
+    {
+        int m = s.length();
+        char[] charr = s.toCharArray();
+        long[] res = new long [m];
+        res[0] = charr[0] - 'a'+1;
+        for (int i = 1; i < charr.length; i++)
+            res[i] = res[i-1] + (charr[i] - 'a'+1) * pow[i];
+        same.put(res[m-1], same.getOrDefault(res[m-1], 0L) +1L);    
+        
+        for(int i = 0; i < m; i++){
+            long h = 0L;
+            if(i == 0){
+                h = res[m - 1] - res[i];
+                h += 27;
+            }else if(i == m - 1){
+                h = res[i - 1];
+                h += 27 * pow[i];
+            }else{
+                h = res[i - 1];
+                h += res[m - 1] - res[i];
+                h += 27 * pow[i];
+            }
+            map.put(h, map.getOrDefault(h, 0L) +1L);
+        }
+    }
 	
 	public static void main(final String[] args) {
-		Integer[] M = {1,4,1,2};
-		System.out.print(visuallyBalancedSections(Arrays.asList(M)));
+		String[] M = {"abc", "abd", "cbd"};
+		System.out.print(strangelyCompatible(Arrays.asList(M)));
 	}
 }
