439b1835e2a5c2c12448af0639444d7252aa8a19
Klimkina Liudmila <klimkina@gmail.com>
2019-03-17 21:06:33 -0700

    Visually Balanced Sections

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -5,26 +5,44 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 class Solution {
 	
-	public static int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int start = 0;
-        while(start < nums.length && nums[start] <=0)
-            start++;
-        if (nums.length == 0 || start >= nums.length || nums[start] > 1)
-            return 1;
-        start++;
-        while (start < nums.length) {
-            if (nums[start] - nums[start-1] > 1)
-                return nums[start-1] + 1;
-            start++;
+	public static int visuallyBalancedSections(List<Integer> colors) {
+	    // Write your code here
+	        long[] cols = new long[colors.size()];
+	        for (int i = 0; i < cols.length; i++)
+	            cols[i] = 1L << colors.get(i);
+	        long res = 1L;
+	        //res += cols.length;
+	        long[] prefix = new long[cols.length];
+	        prefix[0] = cols[0];
+	        for (int i = 1; i < cols.length; i++)
+	        {
+	            prefix[i] = prefix[i-1] ^ cols[i];
+	            if (isBalanced(prefix[i]))
+	            	res++;
+	        }
+	        
+	        for (int i = 0; i < cols.length - 1; i++)
+	        	for (int j = i+1; j < cols.length; j++)
+	        		if (isBalanced(prefix[i]^prefix[j]))
+		            	res++;
+	        return (int)res;
+	    }
+
+	private static boolean isBalanced(long l)
+	{
+		for (long c = 1L; c <= (1L << 50); c = c << 1)
+        {
+            if ((l ^ c) == 0L)
+                return true;
         }
-        return nums[start-1] + 1;
-    }
+		return (l == 0L);
+	}
 	
 	public static void main(final String[] args) {
-		int[] M = {1,1,3};
-		System.out.print(firstMissingPositive(M));
+		Integer[] M = {1,4,1,2};
+		System.out.print(visuallyBalancedSections(Arrays.asList(M)));
 	}
 }
