b20b5d0065a96bc1c4856c3880154cc431d339e9
Klimkina Liudmila <klimkina@gmail.com>
2019-03-22 15:56:10 -0700

    Find K-th Smallest Pair Distance

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,5 +1,7 @@
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -7,50 +9,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 class Solution {
 	
-	public static String minWindow(String s, String t) {
-		int min_start = -1;
-        int min_len = -1;
-        HashMap<Character, Integer> set = new HashMap<>();
-        char[] charr = t.toCharArray();
-        int counter = charr.length;
-        for (int i = 0; i < charr.length; i++)
-            set.put(charr[i], set.getOrDefault(charr[i], 0)+1);
-        charr = s.toCharArray();
-        int begin=0, end=0;
-        while (end < charr.length)
-        {            
-            if(set.get(charr[end]) != null) {
-                set.put(charr[end], set.get(charr[end]) - 1);
-                if (set.get(charr[end]) >= 0)
-                	counter--;
-                
-            }
-            end++;
-            while (counter == 0)
-            {
-                if (min_len == -1 || end - begin < min_len)
-                {
-                    min_len = end - begin;
-                    min_start = begin;
-                }
-                if (set.get(charr[begin]) != null) {
-                	set.put(charr[begin], set.get(charr[begin]) + 1);
-	                if (set.get(charr[begin]) == 1)
-	                	counter++;
-                }
-            	begin++;                      
-            }   
+	public int smallestDistancePair(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int[] arr = new int[nums[n-1]+1];
+        for (int i = 0; i < n-1; i++)
+            for (int j = i+1; j < n; j++)
+                arr[Math.abs(nums[i] - nums[j])]++;
+        int sum = 0;
+        for (int i = 0; i < nums[n-1]; i++){
+            if (sum + arr[i] >= k)
+                return i;
+            else
+                sum += arr[i];
         }
-        if (min_len == -1)
-            return "";
-        return s.substring(min_start, min_len+min_start);
+        return -1;
     }
 	
 	public static void main(final String[] args) {
-		String s = "ADOBECODEBANC";
-		String t = "ABC";
-		System.out.print(minWindow(s,t));
+		int[][] grid = {{1,2,0},{0,0,0},{0,0,0}};
+		System.out.print(shortestDistance(grid));
 	}
 }
