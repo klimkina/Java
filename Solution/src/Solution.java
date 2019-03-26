4a53e573a5de60012aad8b45f8133281e6329707
Klimkina Liudmila <klimkina@gmail.com>
2019-03-25 21:49:58 -0700

    next greater element

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,40 +1,52 @@
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Stack;
+import java.util.TreeMap;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 
 
 class Solution {
-	public static  int[] findDiagonalOrder(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[] res = new int[m*n];
-        int pos_x = 0;
-        int pos_y = 0;
-        int dir = 1;
-        for (int i = 0; i < res.length; i++) {
-            res[i] = matrix[pos_x][pos_y];
-            if ( dir > 0) { // moving up
-                if      (pos_y == m-1) { pos_x++; dir = -dir;}
-                else if (pos_x == 0)     { pos_y++; dir = -dir;}
-                else            { pos_x-=dir; pos_y+=dir; }
-            } else {                // moving down
-                if      (pos_x == n - 1) { pos_y++; dir = -dir; }
-                else if (pos_y == 0)     { pos_x++; dir = -dir; }
-                else            { pos_x-=dir; pos_y+=dir; }
-            }   
+	public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i = nums2.length - 1; i >= 0; i--)
+        {
+            stack.push(nums2[i]);
+            if (i == nums2.length - 1)
+            	map.put(nums2[i], -1);
+            else
+            {
+            	int last = -1;
+            	while (!stack.isEmpty())
+            	{
+            		if (nums2[i] >= stack.peek()) stack.pop();
+            		else
+            		{
+            			last = stack.pop();
+            			break;
+            		}
+            	}
+            	map.put(nums2[i], last);
+    			stack.push(nums2[i]);
+            }
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++)
+        {
+            res[i] = map.getOrDefault(nums1[i], -1);
         }
         return res;
     }
 	
 	public static void main(final String[] args) {
-		int[][] matrix = {{1,2,3},
-				{4,5,6},
-				{7,8,9}};
+		int[] nums1 = {4,1,2};
+		int[] nums2 = {1,3,4,2};
 		
-		int[] res = findDiagonalOrder(matrix);
+		int[] res = nextGreaterElement(nums1, nums2);
 		for (int i = 0; i < res.length; i++)
 			System.out.print(res[i] + " ");
 		//System.out.print(queryString("110101011011000011011111000000", 15));
