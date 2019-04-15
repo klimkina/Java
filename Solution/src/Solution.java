ce69a58cff3efd5c89ecf23aaf7c6ce908032152
Klimkina Liudmila <klimkina@gmail.com>
2019-04-14 19:54:14 -0700

    Longest Arithmetic Sequence

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.Stack;
 import java.util.TreeMap;
 import java.util.ArrayList;
@@ -10,46 +11,27 @@ import java.util.HashMap;
 
 
 class Solution {
-	
-	private static boolean enclosed = true;
-    public static int numEnclaves(int[][] A) {
-        int n = A.length;
-        if (n == 0)
-            return 0;
-        int m = A[0].length;
-        int[][] visited = new int[n][m];
-        int counter = 0;
-        for (int i = 0 ; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (A[i][j] == 1 && visited[i][j] == 0)
-                {
-                    enclosed = true;
-                    int t = dfs(A, visited, i, j);
-                    if (enclosed)
-                        counter += t;
-                }
-        return counter;
-    }
-    private static int dfs(int[][] A, int[][] visited, int i, int j)
-    {
-        int[] dx = {-1,0, 1,0};
-        int[] dy = {0, -1, 0, 1};
-        int count = 1;
-        visited[i][j] = 1;
-        for (int k = 0; k < 4; k++)
-        {
-            int new_x = i + dx[k];
-            int new_y = j + dy[k];
-            if (new_x < 0 || new_y < 0 || new_x >= A.length || new_y >= A[0].length)
-                enclosed = false;
-            else if (A[new_x][new_y] == 1 && visited[new_x][new_y] == 0)
-                count += dfs(A, visited, new_x, new_y);
+	public static int longestArithSeqLength(int[] A) {
+		int res = 0;
+        Map<Integer, Integer>[] dp = new HashMap[A.length];
+        for(int i = 0; i < A.length; i++){
+            dp[i] = new HashMap<>();
         }
-        return count;
+        for(int i = A.length-1; i >= 0; i--){
+            for(int j = i+1; j < A.length; j++){
+                int diff = A[i] - A[j];
+                int len = dp[j].containsKey(diff)? dp[j].get(diff)+1: 2;
+                dp[i].put(diff, Math.max(len, dp[i].getOrDefault(diff, 0)));
+                res = Math.max(res, len);
+            }
+        }
+        return res;
+        
     }
+	
 	
 	public static void main(final String[] args) {
-		int[][] arr = {{0,0,0,0},{1,0,1,0},{0,1,1,0},{0,0,0,0}};
-		System.out.print(numEnclaves(arr));
+		int[] A = {3,6,9,12};
+		System.out.print(longestArithSeqLength(A));
 	}
 }
