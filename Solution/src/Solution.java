a3fde6547c0b5ea7549d6b517ba8a8b32e2b663f
Klimkina Liudmila <klimkina@gmail.com>
2019-04-21 20:33:09 -0700

    Number of Islands II

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,60 +1,54 @@
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeMap;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-
-
+import java.util.List;
 
 class Solution {
-	public static class TreeNode {
-		      int val;
-		      TreeNode left;
-		      TreeNode right;
-		      TreeNode(int x) { val = x; }
-		  }
-		 
-		
-	    public static TreeNode recoverFromPreorder(String S) {
-	    	Stack<TreeNode> stack = new Stack<>();
-	    	int pos = 0;
-	    	int n = S.length();
-	    	while (pos < n)
-	    	{
-	    		int lev = 0;
-	    		while(S.charAt(pos) == '-')
-	    		{
-	    			pos++;
-	    			lev++;
-	    		}
-	    		while (!stack.isEmpty() && stack.size() > lev)
-	    			stack.pop();
-	    		StringBuilder sb = new StringBuilder();
-	    		while(pos < n && S.charAt(pos) != '-')
-	    			sb.append(S.charAt(pos++));
-	    		TreeNode node = new TreeNode(Integer.valueOf(sb.toString()));
-	    		if (!stack.isEmpty())
-	    		{
-	    			TreeNode par = stack.peek();
-	    			if (par.left == null)
-	    				par.left = node;
-	    			else
-	    				par.right = node;
-	    		}
-	    		stack.push(node);
-	    	}
-	    	TreeNode res = stack.pop();
-	    	while(!stack.isEmpty())
-	    		res = stack.pop();
-	        return res;
-	    }
-	
+	public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        List<Integer> res = new ArrayList<>();
+        int size = 0;
+        int[] union = new int[m * n +1]; 
+        for (int i = 0; i < positions.length; i++)
+        {
+            int pos = positions[i][0]*n + positions[i][1] + 1;
+            add(pos, union);
+            size++;
+            if (pos - n > 0 && union[pos-n] > 0)
+                size += connect(pos, pos-n, union);
+            if (pos + n <= m*n && union[pos + n] > 0)
+                size += connect(pos, pos+n, union);
+            if (positions[i][1] < n-1 && union[pos + 1] > 0)
+                size += connect(pos, pos+1, union);
+            if (positions[i][1] > 0 && union[pos - 1] > 0)
+                size += connect(pos, pos-1, union);
+            res.add(size);
+        }
+        return res;
+    }
+    private void add(int pos, int[] union)
+    {
+        union[pos] = pos;
+    }
+    private int connect(int pos1, int pos2, int[] union)
+    {
+    	int par1 = find(pos1, union);
+    	int par2 = find(pos2, union);
+        if (par1 != par2)
+        {
+            union[par1] = par2;            
+            return -1;
+        }
+        return 0;
+    }
+    private int find(int pos, int[] union)
+    {
+        while (union[pos] != pos)
+        {
+        	union[pos] = union[union[pos]];
+            pos = union[pos];
+        }
+        return pos;
+    }
 	
 	public static void main(final String[] args) {
-		System.out.print(recoverFromPreorder("1-2--3---4-5--6---7"));
+		//System.out.print(recoverFromPreorder("1-2--3---4-5--6---7"));
 	}
 }
