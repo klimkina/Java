94c616f3882c086c5279352345da3f4c3684c1ef
Klimkina Liudmila <klimkina@gmail.com>
2019-04-25 21:14:00 -0700

    Search Autocomplete System

---
+++ Solution/src/AutocompleteSystem.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+class AutocompleteSystem {
+	class Trie
+    {
+        int inputs = 0;
+        HashMap<Character, Trie> kids = new HashMap<>();
+        String sentence = "";
+    }
+    private void add(String s, int rep)
+    {
+        char[] charr = s.toCharArray();
+        Trie node = root;
+        for (int i = 0; i < charr.length; i++)
+        {
+            if (node.kids.get(charr[i]) == null)
+                node.kids.put(charr[i], new Trie());
+            node = node.kids.get(charr[i]);
+        }
+        node.inputs += rep;
+        node.sentence = s;
+        
+    }
+    private List<String> find(String s)
+    {
+        List<String> res = new ArrayList<>();
+        char[] charr = s.toCharArray();
+        Trie node = root;
+        for (int i = 0; i < charr.length; i++)
+        {
+            if (node.kids.get(charr[i]) == null)
+                return res;
+            node = node.kids.get(charr[i]);
+        }
+        PriorityQueue<Trie> pq = new PriorityQueue<Trie>(3, new Comparator<Trie>() {
+            public int compare(Trie t1, Trie t2) {
+                if (t2.inputs != t1.inputs)
+                    return t2.inputs - t1.inputs;
+                else
+                    return t1.sentence.compareTo(t2.sentence);
+            }
+        });
+        Stack<Trie> stack = new Stack<>();
+        stack.push(node);
+        
+        while (!stack.isEmpty())
+        {
+            node = stack.pop();
+            if (node != null)
+            {
+                
+                if (node.inputs > 0)
+                {
+                    //System.out.println(node.sentence + " " + node.inputs);
+                    pq.offer(node);
+                }
+                
+                for (char ch: node.kids.keySet())
+                {
+                    //System.out.println(ch + " " + node.kids.get(ch).inputs);
+                    
+                    stack.push(node.kids.get(ch));
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++)
+            if (!pq.isEmpty())
+            {
+                String str = pq.poll().sentence;
+                //System.out.println(str);
+                res.add(str);
+            }
+        return res;
+    }
+    
+    private StringBuilder sb = new StringBuilder();
+    private Trie root = new Trie();
+    public AutocompleteSystem(String[] sentences, int[] times) {
+        for (int i = 0; i < sentences.length; i++)
+            add(sentences[i], times[i]);
+    }
+    
+    public List<String> input(char c) {
+        if (c == '#')
+        {
+            add(sb.toString(), 1);
+            sb.setLength(0);
+            return new ArrayList<String>();
+        }
+        sb.append(c);
+        return find(sb.toString());
+        
+    }
+    
+    public static void main(final String[] args) {
+    	String[] sents = {"i love you","island","iroman","i love leetcode"};
+    	int[] times = {5,3,2,2};
+    	AutocompleteSystem obj = new AutocompleteSystem(sents, times);
+    	List<String> l = obj.input('i');
+		System.out.print(l.get(0));
+	}
+}
--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,54 +1,50 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 class Solution {
-	public List<Integer> numIslands2(int m, int n, int[][] positions) {
-        List<Integer> res = new ArrayList<>();
-        int size = 0;
-        int[] union = new int[m * n +1]; 
-        for (int i = 0; i < positions.length; i++)
-        {
-            int pos = positions[i][0]*n + positions[i][1] + 1;
-            add(pos, union);
-            size++;
-            if (pos - n > 0 && union[pos-n] > 0)
-                size += connect(pos, pos-n, union);
-            if (pos + n <= m*n && union[pos + n] > 0)
-                size += connect(pos, pos+n, union);
-            if (positions[i][1] < n-1 && union[pos + 1] > 0)
-                size += connect(pos, pos+1, union);
-            if (positions[i][1] > 0 && union[pos - 1] > 0)
-                size += connect(pos, pos-1, union);
-            res.add(size);
-        }
+	static class Worker
+	{
+		int q;
+		int w;
+		public Worker(int q1, int w1)
+		{
+			q = q1;
+			w = w1;
+		}
+	}
+	public static double mincostToHireWorkers(int[] quality, int[] wage, int K) {
+		int n = quality.length;
+		Worker[] workers = new Worker[n];
+		for (int i = 0; i < n; i++)
+			workers[i] = new Worker(quality[i], wage[i]);
+		Arrays.sort(workers, (a,b) -> ((int)((double)a.w/a.q - (double)b.w/b.q)));
+		
+		int sum = 0;
+		double res = 0;
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i = 0; i < n; i++)
+		{
+			sum += workers[i].q;
+			pq.offer(-workers[i].q);
+			if (i == K-1)
+				res = (double)sum*workers[i].w/workers[i].q;
+			else if (i >= K)
+			{
+				sum += pq.poll();
+				res = Math.min(res, (double)sum*workers[i].w/workers[i].q);
+			}
+		}
         return res;
     }
-    private void add(int pos, int[] union)
-    {
-        union[pos] = pos;
-    }
-    private int connect(int pos1, int pos2, int[] union)
-    {
-    	int par1 = find(pos1, union);
-    	int par2 = find(pos2, union);
-        if (par1 != par2)
-        {
-            union[par1] = par2;            
-            return -1;
-        }
-        return 0;
-    }
-    private int find(int pos, int[] union)
-    {
-        while (union[pos] != pos)
-        {
-        	union[pos] = union[union[pos]];
-            pos = union[pos];
-        }
-        return pos;
-    }
+    
 	
 	public static void main(final String[] args) {
-		//System.out.print(recoverFromPreorder("1-2--3---4-5--6---7"));
+		int[] qual = {25,68,35,62,52,57,35,83,40,51};
+		int[] wage = {147,97,251,129,438,443,120,366,362,343};
+		int K =	2;
+		System.out.print(mincostToHireWorkers(qual, wage, K));
 	}
 }
