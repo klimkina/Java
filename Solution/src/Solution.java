59ac54f5ca3ed052a00bd60f49740582f1480cf8
Klimkina Liudmila <klimkina@gmail.com>
2019-05-05 07:38:50 -0700

    minimum score triangulation

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -10,85 +10,29 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.TreeMap;
 
+/*
+Given N, consider a convex N-sided polygon with vertices labelled A[0], A[i], ..., A[N-1] in clockwise order.
+
+Suppose you triangulate the polygon into N-2 triangles.  For each triangle, the value of that triangle is the product of the labels of the vertices, and the total score of the triangulation is the sum of these values over all N-2 triangles in the triangulation.
+
+Return the smallest possible total score that you can achieve with some triangulation of the polygon.
+ */
+
 class Solution {
-	HashMap<String, Integer> dist_map = new HashMap<>();
-	public int[][] kClosest(int[][] points, int K) {
-        if (points == null || points.length == 0)
-            return points;
-        if (K > points.length)
-            K = points.length;
-        int[][] res = new int[K][2];
-        for(int i = 0; i < K; i++)
-        {
-            res[i] = points[i];
-            swim(res, i, K);
-        }
-        for (int i = K; i < points.length; i++)
-        {
-            if (dist(res[0]) > dist(points[i]))
+	public int minScoreTriangulation(int[] A) {
+        int res = 0;
+        int n = A.length;
+        int[][] dp = new int[n][n];
+        for (int i = 2; i < n; i++)
+            for (int j = 0; j + i < n; j++)
             {
-                res[0] = points[i];
-                sink(res, 0, K);
+                int m = j + i;
+                dp[j][m] = Integer.MAX_VALUE;
+                for (int k = j +1; k < m; k++)
+                    dp[j][m] = Math.min(dp[j][m], dp[j][k] + dp[k][m] + A[j]*A[k]*A[m]);
             }
-        }
-        return res;
         
-    }
-    private void sink(int[][] res, int n, int K)
-    {
-        int dist = dist(res[n]);
-        while (2*n+1 < K)
-        {
-            int kid = n*2+1; //left child
-            int dist1 = dist(res[kid]); //right child if exist
-            int dist2 = Integer.MIN_VALUE;
-            if (kid + 1 < K)
-            	dist2 = dist(res[kid+1]);
-            if(dist1 < dist2)
-                kid++;
-            if (dist > Math.max(dist1, dist2))
-                break;
-            swap(res, n, kid);           
-            n = kid;
-        }
-    }
-    private void swim(int[][] res, int n, int K)
-    {
-        int dist = dist(res[n]);
-        while (n > 0)
-        {
-            int d = dist(res[(n-1)/2]); //parent
-            if (d < dist)
-            {
-                swap(res, n, (n-1)/2);
-                n = (n-1)/2;
-            }
-            else
-                break;
-        }
-    }
-    private void swap(int[][] res, int a, int b)
-    {
-        int[] t = res[a];
-        res[a] = res[b];
-        res[b] = t;
-    }
-    
-    private int dist(int[] point)
-    {
-    	return point[0]*point[0] + point[1]*point[1];
-    }
-    
-    private void print(int[][] res, int K)
-    {
-        System.out.print("[");
-        for (int i = 0; i < K; i++)
-            System.out.print("[" + res[i][0] + "," +res[i][1] +"]");        
-        System.out.println("]");
-        for (int i = 0; i < K; i++)
-            System.out.print(dist(res[i]) + " ");            
-        
-        System.out.println();
+        return dp[0][n-1];
     }
     
 	public static void main(final String[] args) {
