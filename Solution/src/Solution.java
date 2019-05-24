97142e18e05a9f70849c7af98f712f6aa75fdb94
Klimkina Liudmila <klimkina@gmail.com>
2019-05-23 21:55:20 -0700

    regex match

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,99 +1,44 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-/*
-Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
 
-Only one letter can be changed at a time.
-Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
-*/
 class Solution {
-	class Trie
-    {
-        Trie[] kids = new Trie[26];
-    }
-    private Trie root = new Trie();
-    private void add(String s)
-    {
-        Trie node = root;
+	public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        boolean[][] matches = new boolean[n+1][m+1];
+        for (int i = 0; i <= n; i++)
+            Arrays.fill(matches[i],false);
         char[] charr = s.toCharArray();
-        for (int i = 0; i < charr.length; i++)
-        {
-            if (node.kids[charr[i] - 'a'] == null)
-                node.kids[charr[i] - 'a'] = new Trie();
-            node = node.kids[charr[i] - 'a'];
-        }
-    }
-    private boolean find(String s)
-    {
-        Trie node = root;
-        char[] charr = s.toCharArray();
-        for (int i = 0; i < charr.length; i++)
-        {
-            if (node.kids[charr[i] - 'a'] == null)
-                return false;
-            node = node.kids[charr[i] - 'a'];
-        }
-        return true;
-    }
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (beginWord == null || endWord == null || !wordList.contains(endWord))
-            return 0;
-        if (beginWord.equals(endWord))
-            return 0;
-        for(String s: wordList)
-            add(s);
-        HashSet<String> visited = new HashSet<>();
-        HashSet<String> q = new HashSet<>();
-        q.add(beginWord);
-        HashSet<String> q2 = new HashSet<>();
-        q2.add(endWord);
-        visited.add(beginWord);
-        visited.add(endWord);
-        int steps = 1;
-        while (!q.isEmpty()&&!q2.isEmpty())
-        {
-            HashSet<String> qu = new HashSet<>();
-            for (String w : q)
+        char[] parr = p.toCharArray();
+        matches[0][0] = true;
+        for (int i = 0; i < m; i++)
+            if (parr[i] == '*' && matches[0][i-1])
+                matches[0][i+1] = true;
+        
+        for (int i = 0; i < n; i++ )
+            for (int j = 0; j < m; j++)
             {
-                char[] word = w.toCharArray();               
-                
-                for (int l = 0; l < word.length; l++)
+                if (charr[i] == parr[j] || parr[j] == '.')
+                    matches[i+1][j+1] = matches[i][j];
+                else if (parr[j] == '*')
                 {
-                    char old = word[l];
-                    for (char ch = 'a'; ch <= 'z'; ch++)
-                    {
-                        word[l] = ch;
-                        String t = String.valueOf(word);
-                        if (q2.contains(t))
-                            return steps +1;
-                        if (!visited.contains(t) && find(t))
-                        {
-                            qu.add(t);
-                            visited.add(t);
-                        }
-                    }
-                    word[l] = old;
+                    if (parr[j-1] != charr[i] && parr[j-1] != '.') //empty
+                        matches[i+1][j+1] = matches[i+1][j-1];
+                    else
+                        matches[i+1][j+1] = matches[i][j+1] || matches[i+1][j] || matches[i+1][j-1];
                 }
+                else
+                    matches[i+1][j+1] = false;
             }
-            steps++;  
-            if (qu.size() > q2.size())
-            {
-                q = q2;
-                q2 = qu;
-            }
-            else
-                q = qu;
-        }
-        return 0;
+        return matches[n][m];
     }
 	public static void main(String[] args) {   	
-
-		String begin = "hit";
-		String end = "cog";
-		String[] dict = {"hot","dot","dog","lot","log","cog"};
-		int res = ladderLength(begin, end, dict);
+		Solution obj = new Solution();
+		System.out.println(obj.isMatch("aaa", "aaaa"));
 	}
 }
