ff2ba1e56e91ac6b74fb74d9ade5b0b8606a6361
Klimkina Liudmila <klimkina@gmail.com>
2019-05-29 15:55:39 -0700

    zero row

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -7,38 +7,56 @@ import java.util.Set;
 
 
 class Solution {
-	public boolean isMatch(String s, String p) {
-        int n = s.length();
-        int m = p.length();
-        boolean[][] matches = new boolean[n+1][m+1];
-        for (int i = 0; i <= n; i++)
-            Arrays.fill(matches[i],false);
-        char[] charr = s.toCharArray();
-        char[] parr = p.toCharArray();
-        matches[0][0] = true;
+	public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean first_row = false;
+        for (int i = 0; i < n; i++)
+            if (matrix[i][0] == 0)
+                first_row = true;
+        boolean first_col = false;
         for (int i = 0; i < m; i++)
-            if (parr[i] == '*' && i > 0 && matches[0][i-1])
-                matches[0][i+1] = true;
-        
-        for (int i = 0; i < n; i++ )
+            if (matrix[0][i] == 0)
+                first_col = true;
+        for (int i = 1; i < n; i++)
             for (int j = 0; j < m; j++)
-            {
-                if (charr[i] == parr[j] || parr[j] == '.')
-                    matches[i+1][j+1] = matches[i][j];
-                else if (parr[j] == '*')
+                if (matrix[i][j] == 0)
                 {
-                    if (j > 0 && parr[j-1] != charr[i] && parr[j-1] != '.') //empty
-                        matches[i+1][j+1] = matches[i+1][j-1];
-                    else
-                        matches[i+1][j+1] = matches[i][j+1] || matches[i+1][j] || (j > 0 && matches[i+1][j-1]);
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
-                else
-                    matches[i+1][j+1] = false;
+        for (int i = 0; i < n; i++)
+            if (matrix[i][0] == 0)
+            {
+                System.out.println("row:" + i);
+                for (int j = 0; j < m; j++)
+                    matrix[i][j] = 0;
             }
-        return matches[n][m];
+        for (int j = 0; j < m; j++)
+            if (matrix[0][j] == 0)
+            {
+                System.out.println("col:" + j);
+                for (int i = 0; i < n; i++)
+                    matrix[i][j] = 0;
+            }
+        if (first_row)
+            for (int j = 1; j < m; j++)
+                matrix[0][j] = 0;
+        if (first_col)
+            for (int i = 1; i < n; i++)
+                matrix[i][0] = 0;
+        if (first_row || first_col)
+            matrix[0][0] = 0;
     }
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		System.out.println(obj.isMatch("aaa", "*aaa"));
+		int[][] matrix = {{1,0,3}};
+		obj.setZeroes(matrix);
+		for (int i = 0 ; i < matrix.length; i++)
+		{
+			for (int j = 0; j < matrix[0].length; j++)
+				System.out.print(matrix[i][j] + " ");
+			System.out.println();
+		}
 	}
 }
