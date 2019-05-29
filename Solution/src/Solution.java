4fe39f44dfb8584f1aebd9146e2fdeb004dc2685
Klimkina Liudmila <klimkina@gmail.com>
2019-05-29 16:09:24 -0700

    atoi

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -7,56 +7,39 @@ import java.util.Set;
 
 
 class Solution {
-	public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        boolean first_row = false;
-        for (int i = 0; i < n; i++)
-            if (matrix[i][0] == 0)
-                first_row = true;
-        boolean first_col = false;
-        for (int i = 0; i < m; i++)
-            if (matrix[0][i] == 0)
-                first_col = true;
-        for (int i = 1; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (matrix[i][j] == 0)
-                {
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
-                }
-        for (int i = 0; i < n; i++)
-            if (matrix[i][0] == 0)
-            {
-                System.out.println("row:" + i);
-                for (int j = 0; j < m; j++)
-                    matrix[i][j] = 0;
-            }
-        for (int j = 0; j < m; j++)
-            if (matrix[0][j] == 0)
-            {
-                System.out.println("col:" + j);
-                for (int i = 0; i < n; i++)
-                    matrix[i][j] = 0;
-            }
-        if (first_row)
-            for (int j = 1; j < m; j++)
-                matrix[0][j] = 0;
-        if (first_col)
-            for (int i = 1; i < n; i++)
-                matrix[i][0] = 0;
-        if (first_row || first_col)
-            matrix[0][0] = 0;
+	public int myAtoi(String str) {
+        int res = 0;
+        if (str == null || str.length() == 0)
+            return res;
+        
+        char[] charr = str.toCharArray();
+        int start = 0;
+        while(start < charr.length && charr[start] == ' ')
+            start++;
+        int sign = 1;
+        if (start < charr.length && (charr[start] == '-' || charr[start] == '+'))
+        {
+            
+            sign = charr[start++] == '-' ? -1 : 1;
+        }
+        
+        while(start < charr.length)
+        {
+            if (charr[start] < '0' || charr[start] > '9')
+                return res*sign;
+            int next = charr[start++] - '0';
+            if (sign > 0 && (Integer.MAX_VALUE/10 < res || Integer.MAX_VALUE - next < res * 10))
+                return Integer.MAX_VALUE;
+            if (sign < 0 && (Integer.MIN_VALUE/10 > - res || Integer.MIN_VALUE + next > -res * 10))
+                return Integer.MIN_VALUE;
+            res = res * 10 + next;
+        }
+        res = sign*res;
+        return res;
     }
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		int[][] matrix = {{1,0,3}};
-		obj.setZeroes(matrix);
-		for (int i = 0 ; i < matrix.length; i++)
-		{
-			for (int j = 0; j < matrix[0].length; j++)
-				System.out.print(matrix[i][j] + " ");
-			System.out.println();
-		}
+		System.out.println(obj.myAtoi("-2147483648"));
+		
 	}
 }
