b4777c045234b17aa286c96ed0fdac9a95c6e36f
Klimkina Liudmila <klimkina@gmail.com>
2019-05-30 17:14:55 -0700

    remove comments

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -7,39 +7,78 @@ import java.util.Set;
 
 
 class Solution {
-	public int myAtoi(String str) {
-        int res = 0;
-        if (str == null || str.length() == 0)
-            return res;
-        
-        char[] charr = str.toCharArray();
-        int start = 0;
-        while(start < charr.length && charr[start] == ' ')
-            start++;
-        int sign = 1;
-        if (start < charr.length && (charr[start] == '-' || charr[start] == '+'))
-        {
-            
-            sign = charr[start++] == '-' ? -1 : 1;
-        }
-        
-        while(start < charr.length)
-        {
-            if (charr[start] < '0' || charr[start] > '9')
-                return res*sign;
-            int next = charr[start++] - '0';
-            if (sign > 0 && (Integer.MAX_VALUE/10 < res || Integer.MAX_VALUE - next < res * 10))
-                return Integer.MAX_VALUE;
-            if (sign < 0 && (Integer.MIN_VALUE/10 > - res || Integer.MIN_VALUE + next > -res * 10))
-                return Integer.MIN_VALUE;
-            res = res * 10 + next;
-        }
-        res = sign*res;
-        return res;
+	public List<String> removeComments(String[] source) {
+		 List<String> res = new ArrayList<>();
+	        if (source == null || source.length == 0)
+	            return res;
+	        boolean block = false;
+	        int i = 0;
+	        String str = source[i];
+	        String prev = "";
+	        while (i < source.length-1 || !str.isEmpty())
+	        {
+	            if (block) // look for closing
+	            {
+	                int start = str.indexOf("*/");
+	                if (start >= 0)
+	                {
+	                    str = prev + str.substring(start+2, str.length());
+	                    block = false;
+	                    prev = "";
+	                }
+	                else
+	                    str = source[++i];
+	            }
+	            else // look for // or /*
+	            {
+	                int start1 = str.indexOf("//");
+	                int start2 = str.indexOf("/*");
+	                if (start1 >= 0 && (start1 < start2 || start2 < 0))
+	                {
+	                    str = str.substring(0, start1);
+	                    if (!str.isEmpty())
+	                        res.add(str);
+	                    if (i < source.length-1)
+	                        str = source[++i];
+	                    else
+	                        str = "";
+	                }
+	                else if (start1 < 0 && start2 < 0)
+	                {
+	                    if (!str.isEmpty())
+	                        res.add(str);
+	                    if (i < source.length-1)
+	                        str = source[++i];
+	                    else
+	                        str = "";
+	                }
+	                else
+	                {
+	                    int end = str.indexOf("*/", start2 + 2);
+	                    if (end >= 0)
+	                        str = str.substring(0, start2) + str.substring(end +2, str.length());
+	                    else
+	                    {
+	                        prev = str.substring(0, start2);
+	                        //if (!str.isEmpty())
+	                        //    res.add(str);
+	                        if (i < source.length-1)
+	                            str = source[++i];
+	                        else
+	                            str = "";
+	                        block = true;
+	                    }
+	                }
+	            }
+	        }
+	        return res;
     }
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		System.out.println(obj.myAtoi("-2147483648"));
+		String[] source = {"/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"};
+		List<String> res = obj.removeComments(source);
+		for (String s : res)
+			System.out.println(s);
 		
 	}
 }
