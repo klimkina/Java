5823323b3a3623dbb13dec8b1584585055c837b5
Klimkina Liudmila <klimkina@gmail.com>
2019-05-31 17:43:16 -0700

    matrix 01 distance

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -7,43 +7,45 @@ import java.util.Set;
 
 
 class Solution {
-	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1 == null || nums2 == null)
-            throw new IllegalArgumentException("The arrays are empty.");
-        if (nums1.length > nums2.length)
-            return findMedianSortedArrays(nums2, nums1);
-        int m = nums1.length;
-        int n = nums2.length;
-        int lo = 0;
-        int hi = m;
-        int count = (m + n + 1)/2;
-        int mid1 = 0;
-        int mid2 = 0;
-        while (lo <= hi)
-        {
-            mid1 = lo + (hi-lo)/2;
-            mid2 = count - mid1;
-            int max1 = mid1 == 0 ? Integer.MIN_VALUE : nums1[mid1-1];
-            int max2 = mid2 == 0 ? Integer.MIN_VALUE : nums2[mid2-1];
-            int min1 = mid1 == m ? Integer.MAX_VALUE : nums1[mid1];
-            int min2 = mid2 == n ? Integer.MAX_VALUE : nums2[mid2];
-            if (max1 > min2)
-                hi = mid1-1;
-            else if (max2 > min1)
-                lo = mid1+1;
-            else
-            {
-                int res = Math.max(max1, max2);
-                if ((m + n) % 2 > 0)
-                    return res;
-                return  (double) (res + Math.min(min1, min2))/2;
+	public int[][] updateMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        if (rows == 0)
+            return matrix;
+        int cols = matrix[0].length;
+        int[] row = new int[cols];
+        int[][] dist = new int[rows][cols];
+        for (int i = 0; i < rows; i++)
+            Arrays.fill(dist[i], Integer.MAX_VALUE - 10000);
+
+        //First pass: check for left and top
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0)
+                    dist[i][j] = 0;
+                else {
+                    if (i > 0)
+                        dist[i][j] = Math.min(dist[i][j], dist[i - 1][j] + 1);
+                    if (j > 0)
+                        dist[i][j] = Math.min(dist[i][j], dist[i][j - 1] + 1);
+                }
             }
         }
-        throw new IllegalArgumentException();
+
+        //Second pass: check for bottom and right
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                if (i < rows - 1)
+                    dist[i][j] = Math.min(dist[i][j], dist[i + 1][j] + 1);
+                if (j < cols - 1)
+                    dist[i][j] = Math.min(dist[i][j], dist[i][j + 1] + 1);
+            }
+        }
+
+        return dist;
     }
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		int[] nums1 = {1,3};
+		int[][] nums1 = {1,3};
 		int[] nums2 = {2};
 		System.out.println(obj.findMedianSortedArrays(nums1, nums2));
 	}
