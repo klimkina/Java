d720931aab1f402cea7003b9aeadc04c6d556be6
Klimkina Liudmila <klimkina@gmail.com>
2019-06-02 08:46:39 -0700

    Number of Submatrices That Sum to Target

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -7,46 +7,36 @@ import java.util.Set;
 
 
 class Solution {
-	public int[][] updateMatrix(int[][] matrix) {
-        int rows = matrix.length;
-        if (rows == 0)
-            return matrix;
-        int cols = matrix[0].length;
-        int[] row = new int[cols];
-        int[][] dist = new int[rows][cols];
-        for (int i = 0; i < rows; i++)
-            Arrays.fill(dist[i], Integer.MAX_VALUE - 10000);
+	public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0)
+            return 0;
+        
+        int n = matrix.length, m = matrix[0].length;
+        int[][] cum = new int[n+1][m+1];
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+                cum[i+1][j+1] = cum[i+1][j] + cum[i][j+1] - cum[i][j] + matrix[i][j];
+            }
+        }
 
-        //First pass: check for left and top
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0)
-                    dist[i][j] = 0;
-                else {
-                    if (i > 0)
-                        dist[i][j] = Math.min(dist[i][j], dist[i - 1][j] + 1);
-                    if (j > 0)
-                        dist[i][j] = Math.min(dist[i][j], dist[i][j - 1] + 1);
+        int ans = 0;
+        for(int i = 0;i < n;i++){
+            for(int j = i;j < n;j++){
+                Map<Integer, Integer> map = new HashMap<>();
+                for(int k = 0;k <= m;k++){
+                    int v = cum[j+1][k] - cum[i][k];
+                    if(map.containsKey(v - target)){
+                        ans += map.get(v-target);
+                    }
+                    map.put(v, map.getOrDefault(v, 0)+1);
                 }
             }
         }
-
-        //Second pass: check for bottom and right
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 0; j--) {
-                if (i < rows - 1)
-                    dist[i][j] = Math.min(dist[i][j], dist[i + 1][j] + 1);
-                if (j < cols - 1)
-                    dist[i][j] = Math.min(dist[i][j], dist[i][j + 1] + 1);
-            }
-        }
-
-        return dist;
+        return ans;
     }
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		int[][] nums1 = {1,3};
-		int[] nums2 = {2};
-		System.out.println(obj.findMedianSortedArrays(nums1, nums2));
+		int[][] nums1 = {{0,1,0},{1,1,1},{0,1,0}};
+		System.out.println(obj.numSubmatrixSumTarget(nums1, 0));
 	}
 }
