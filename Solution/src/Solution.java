408edab3e2c0c4427a80e849da12bd341cefe915
Klimkina Liudmila <klimkina@gmail.com>
2019-06-09 11:33:55 -0700

    Letter Tile Possibilities

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -12,50 +12,46 @@ import java.util.TreeMap;
 
 
 class Solution {
-	public String smallestSubsequence(String text) {
-		TreeMap<Character, Integer> map = new TreeMap<>();
-        TreeMap<Integer, Character> reverse = new TreeMap<>();
-        char[] charr = text.toCharArray();
-        for (int i = 0; i < charr.length; i++)
-            map.put(charr[i], i);
-        for (char ch: map.keySet())
-            reverse.put(map.get(ch), ch);
-        StringBuilder sb = new StringBuilder();
-        int left = 0;
-        while(!map.isEmpty())
-        {
-            int right = reverse.firstEntry().getKey();
-            char ch = reverse.get(right); 
-            if (!map.containsKey(ch)) // already used this
-                reverse.remove(right);
+	public int numTilePossibilities(String tiles) {
+        char[] charr = tiles.toCharArray();
+        Arrays.sort(charr);
+        HashSet<String> used = new HashSet<>(); // strings with already calculated permutations
+        return calcPerm(String.valueOf(charr), used);
+    }
+    private int calcPerm(String s, HashSet<String> used)
+    {
+        if (s.length() < 1 || used.contains(s))
+            return 0; 
+        used.add(s);
+        if (s.length() == 1)
+            return 1;
+        int res = perms(s);
+        for (int i = 0; i < s.length(); i++)
+            if(i == 0 || s.charAt(i) != s.charAt(i-1))//all original substrings of s
+                res += calcPerm(s.substring(0, i) + s.substring(i +1, s.length()), used);
+        return res;
+    }
+    private int perms(String s) //number of permutations of a string n!/m1!m2!...mk!
+    {
+        int res = fac(s.length());
+        int len = 1;
+        for (int i = 1; i < s.length(); i++)
+            if (s.charAt(i) == s.charAt(i-1))
+                len++;
             else
             {
-                char min = ch; // if we can find smaller letter before
-                int min_pos = right; //min pos of this letter
-                int i = left;
-                left = right + 1;
-                for (; i < right; i++)
-                {
-                    char c = text.charAt(i);
-                    if (map.containsKey(c) && c < min)
-                    {
-                        min = c;
-                        left = i + 1;
-                    }
-                    else if (c == ch && i < min_pos)
-                        min_pos = i;
-                }
-                if (min < ch) //found smaller letter
-                	ch = min;
-                else
-                	left = min_pos+1; //found smaller pos
-                if (reverse.containsKey(left-1))
-                    reverse.remove(left-1);
-                map.remove(ch);
-                sb.append(ch);
+                res /= fac(len); // divide for repeated characters
+                len = 1;
             }
-        }
-        return sb.toString();
+        res /= fac(len);
+        return res;
+    }
+    private int fac(int n)
+    {
+        int res = 1;
+        for (int i = 2; i <=n; i++)
+            res *=i;
+        return res;
     }
     
 	public static void main(String[] args) {   	
