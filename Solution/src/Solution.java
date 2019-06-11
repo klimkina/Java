a012c1102b65bcb3763db4b1bb202c3e2d91e6eb
Klimkina Liudmila <klimkina@gmail.com>
2019-06-11 14:39:53 -0700

    is list palindrome

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -12,116 +12,55 @@ import java.util.TreeMap;
 
 
 class Solution {
-	public int[][] candyCrush(int[][] board) {
-        while(crush(board))
-            compress(board);
-        return board;
-    }
-    private boolean crush(int[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-        int[][] next = new int[m][n];
-        boolean res = false;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                next[i][j] = board[i][j];
-        for(int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-            	if (board[i][j] != 0)
-	            {
-	                if (checkHor(board, i, j))
-	                {
-	                    res = true;
-	                    next[i][j] = 0;
-	                    int nx = j-1;
-	                    while (nx >= 0)
-	                        if (board[i][nx] == board[i][j])
-	                            next[i][nx--] = 0;
-	                        else
-	                            break;
-	                    nx = j+1;
-	                    while (nx < board[0].length)
-	                        if (board[i][nx] == board[i][j])
-	                            next[i][nx++] = 0;
-	                        else
-	                            break;
-	                }
-	                if (checkVert(board, i, j))
-	                {
-	                    res = true;
-	                    int nx = i-1;
-	                    while (nx >= 0)
-	                        if (board[nx][j] == board[i][j])
-	                            next[nx--][j] = 0;
-	                        else
-	                            break;
-	                    nx = i+1;
-	                    while (nx < board.length)
-	                        if (board[nx][j] == board[i][j])
-	                            next[nx++][j] = 0;
-	                        else
-	                            break;
-	                }
-	            }
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] = next[i][j];
-        return res;
-    }
-    private boolean checkHor(int[][] board, int row, int col)
-    {
-        int count = 1;
-        int next = col-1;
-        while (next >= 0)
-            if (board[row][next--] == board[row][col])
-                count++;
-            else
-                break;
-        next = col+1;
-        while (next < board[0].length)
-            if (board[row][next++] == board[row][col])
-                count++;
-            else
-                break;
-        return count >= 3;
-    }
-    private boolean checkVert(int[][] board, int row, int col)
-    {
-        int count = 1;
-        int next = row-1;
-        while (next >= 0)
-            if (board[next--][col] == board[row][col])
-                count++;
-            else
-                break;
-        next = row+1;
-        while (next < board.length)
-            if (board[next++][col] == board[row][col])
-                count++;
-            else
-                break;
-        return count >= 3;
-    }
-    private void compress(int[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-        for(int i = 0; i < n; i++)
+	public static class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; }
+		 }
+	public boolean isPalindrome(ListNode head) {
+        if (head == null)
+            return true;
+        ListNode node = head;
+        int count = 0;
+        while (node != null)
         {
-            int low = m-1;
-            int curr = m-1;
-            while(curr >= 0)
-                if (board[curr][i] != 0)
-                    board[low--][i] = board[curr--][i];
-                else
-                    curr--;
-            while (low >= 0)
-                board[low--][i] = 0;
+            node = node.next;
+            count++;
         }
+        node = head;
+        ListNode prev = node;
+        for (int i = 0; i < count/2; i++)
+        {
+            prev = node;
+            node = node.next;
+        }
+        
+        while(node.next != null)
+        {
+            ListNode next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        if (node != null)
+            node.next = prev;
+        for (int i = 0; i < count/2; i++)
+        {
+            if (node.val != head.val)
+                return false;
+            node = node.next;
+            head = head.next;
+        }
+        return true;
     }
     
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		int[][] board = {{110,5,112,113,114},{210,211,5,213,214},{310,311,3,313,314},{410,411,412,5,414},{5,1,512,3,3},{610,4,1,613,614},{710,1,2,713,714},{810,1,2,1,1},{1,1,2,2,2},{4,1,4,4,1014}};
-		obj.candyCrush(board);
+		ListNode list = new ListNode(1);
+		ListNode list1 = new ListNode(2);
+		ListNode list2 = new ListNode(2);
+		ListNode list3 = new ListNode(1);
+		list.next = list1; list1.next = list2; list2.next = list3;
+		obj.isPalindrome(list);
 	}
 }
