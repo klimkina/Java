53eb69067b5b3904767e2be30ca140000638b49c
Klimkina Liudmila <klimkina@gmail.com>
2019-06-11 15:26:50 -0700

    Flatten a Multilevel Doubly Linked List

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -12,55 +12,69 @@ import java.util.TreeMap;
 
 
 class Solution {
-	public static class ListNode {
-		      int val;
-		      ListNode next;
-		      ListNode(int x) { val = x; }
-		 }
-	public boolean isPalindrome(ListNode head) {
-        if (head == null)
-            return true;
-        ListNode node = head;
-        int count = 0;
-        while (node != null)
+	class Node {
+	    public int val;
+	    public Node prev;
+	    public Node next;
+	    public Node child;
+
+	    public Node() {}
+
+	    public Node(int _val,Node _prev,Node _next,Node _child) {
+	        val = _val;
+	        prev = _prev;
+	        next = _next;
+	        child = _child;
+	    }
+	};
+	public Node flatten(Node head) {
+        Node node = head;
+        node = flat(node);
+        //printNext(head);
+        //printPrev(node);
+        return head;
+    }
+    public Node flat(Node head) {
+        while (head != null)
         {
-            node = node.next;
-            count++;
+            if (head.child != null)
+            {
+                Node next = head.next;
+                head.next = head.child;
+                head.child.prev = head;
+                head.child = null;
+                head = flat(head.next);
+                head.next = next;
+                if (next != null)
+                    next.prev = head;
+            }
+            if (head.next == null)
+                return head;
+            else
+                head = head.next;
         }
-        node = head;
-        ListNode prev = node;
-        for (int i = 0; i < count/2; i++)
+        return head;
+    }
+    private void printNext(Node head)
+    {
+        while (head != null)
         {
-            prev = node;
-            node = node.next;
-        }
-        
-        while(node.next != null)
-        {
-            ListNode next = node.next;
-            node.next = prev;
-            prev = node;
-            node = next;
-        }
-        if (node != null)
-            node.next = prev;
-        for (int i = 0; i < count/2; i++)
-        {
-            if (node.val != head.val)
-                return false;
-            node = node.next;
+            System.out.print(head.val + "->");
             head = head.next;
         }
-        return true;
+        System.out.println();
+    }
+    private void printPrev(Node head)
+    {
+        while (head != null)
+        {
+            System.out.print(head.val + "->");
+            head = head.prev;
+        }
     }
     
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		ListNode list = new ListNode(1);
-		ListNode list1 = new ListNode(2);
-		ListNode list2 = new ListNode(2);
-		ListNode list3 = new ListNode(1);
-		list.next = list1; list1.next = list2; list2.next = list3;
-		obj.isPalindrome(list);
+		//System.out.println(obj.frequencySort("tree"));
 	}
 }
