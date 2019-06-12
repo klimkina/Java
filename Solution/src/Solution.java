6a9aed1f9de484a5971f5ef665aeb6d6aa8ea577
Klimkina Liudmila <klimkina@gmail.com>
2019-06-11 18:41:57 -0700

    random node

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -6,41 +6,49 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Random;
 import java.util.Set;
 import java.util.Stack;
 import java.util.TreeMap;
 
 
 class Solution {
-	public List<List<Integer>> subsets(int[] nums) {
-        return subsets(nums, 0);
-    }
-    private List<List<Integer>> subsets(int[] nums, int start)
-    {
-    	List<List<Integer>> res = new ArrayList<>();
-        if (start == nums.length)
-            res.add(new ArrayList<>());
-        else
-        {
-	        int i = nums[start];
-	        List<List<Integer>> sub = subsets(nums, start+1);
-	        for (List<Integer> l : sub)
-	        {
-	        	List<Integer> t = new ArrayList<>();
-	        	t.addAll(l);
-	            l.add(i);
-	            res.add(t);
-	            res.add(l);
-	        }
-        }
-        
-        return res;
-    }
+	public class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; }
+		 }
+	    /** @param head The linked list's head.
+	        Note that the head is guaranteed to be not null, so it contains at least one node. */
+	    ListNode head;
+	    Random rand = new Random(); 
+	    public Solution(ListNode head) {
+	        this.head = head;
+	    }
+	    
+	    /** Returns a random node's value. */
+	    public int getRandom() {
+	        ListNode node = head;
+	        // Initialize result as first node 
+	        int result = node.val;   
+	        for (int n = 1; node != null; n++) { 
+	  
+	            // change result with probability 1/n 
+	            if (rand.nextInt(n) == 0) { 
+	                result = node.val; 
+	            } 
+	  
+	            // Move to next node 
+	            node = node.next; 
+	        } 
+	        return result;
+	    }
+	}
+  
     
 	public static void main(String[] args) {   	
-		Solution obj = new Solution();
+		Solution obj = new Solution(new ListNode(0));
 		int[] nums = {1,2,3};
-		obj.subsets(nums);
 		//System.out.println(obj.frequencySort("tree"));
 	}
 }
