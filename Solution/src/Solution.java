356053dbeb224a8e725e587db76464bdc52e7796
Klimkina Liudmila <klimkina@gmail.com>
2019-07-09 19:43:21 -0700

    reorder list

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -18,33 +18,51 @@ import java.util.TreeSet;
 
 
 class Solution {
-	HashMap<Integer, Integer> map = new HashMap<>();
-    public int minHeightShelves(int[][] books, int shelf_width) {
-        return minHeightShelves(books, shelf_width, 0);
-    }
-    private int minHeightShelves(int[][] books, int shelf_width, int pos) {
-        if (pos >= books.length)
-            return 0;
-        if (map.containsKey(pos))
-            return map.get(pos);
-        int currW = 0;
-        int maxH = 0;
-        int minTotalH = Integer.MAX_VALUE;
-        int i = pos;
-        while (i < books.length && currW + books[i][0] <= shelf_width)
+	public class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; }
+		  }
+	public void reorderList(ListNode head) {
+        if (head != null)
         {
-            currW = currW + books[i][0];
-            maxH = Math.max(maxH, books[i][1]);
-            minTotalH = Math.min(minTotalH, maxH + minHeightShelves(books, shelf_width, i+1));
-            i++;
+            ListNode slow = head;
+            ListNode fast = head;
+            //find mid
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next;
+                if (fast.next != null)
+                    fast = fast.next;
+            }
+            ListNode prev = null;            
+            //reverse end
+            while (slow != null)
+            {
+                ListNode node = slow.next;
+                slow.next = prev;
+                prev = slow;
+                slow = node;
+                
+            }
+            ListNode left = head;
+            ListNode right = prev;
+            while (left != null && right != null && right.next != null)
+            {
+                //System.out.println(left.val + " " + right.val);
+                ListNode node = right.next;
+                right.next = left.next;
+                left.next = right;
+                left = right.next;
+                right = node;
+            }
         }
-        map.put(pos, minTotalH);
-        return minTotalH;
     }
     
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
 		int[][] books = {{9,9},{5,4},{3,1},{1,5},{7,3}};
-		System.out.println(obj.minHeightShelves(books, 10));
+		//System.out.println(obj.minHeightShelves(books, 10));
 	}
 }
