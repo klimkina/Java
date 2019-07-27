8a1de7d00d95785d8549d69cac6e8c4382df4aeb
Klimkina Liudmila <klimkina@gmail.com>
2019-07-27 09:30:10 -0700

    min city roads

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -16,43 +16,52 @@ import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-/*
-Given an array arr of positive integers, consider all binary trees such that:
 
-Each node has either 0 or 2 children;
-The values of arr correspond to the values of each leaf in an in-order traversal of the tree.  (Recall that a node is a leaf if and only if it has 0 children.)
-The value of each non-leaf node is equal to the product of the largest leaf value in its left and right subtree respectively.
-Among all possible binary trees considered, return the smallest possible sum of the values of each non-leaf node.  It is guaranteed this sum fits into a 32-bit integer.
- */
 class Solution {
-	public int mctFromLeafValues(int[] A) {
-        int res = 0, n = A.length;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(Integer.MAX_VALUE);
-        for (int a : A) {
-            while (stack.peek() <= a) {
-                int mid = stack.pop();
-                res += mid * Math.min(stack.peek(), a);
-            }
-            stack.push(a);
+	public int minimumCost(int N, int[][] conections) {
+        HashMap<Integer, HashMap<Integer, Integer>> graph = new HashMap<>();
+        for (int i = 0; i < conections.length; i++)
+        {
+            int[] conn = conections[i];
+            if (!graph.containsKey(conn[0]))
+                graph.put(conn[0], new HashMap<>());
+            graph.get(conn[0]).put(conn[1], conn[2]);
+            if (!graph.containsKey(conn[1]))
+                graph.put(conn[1], new HashMap<>());
+            graph.get(conn[1]).put(conn[0], conn[2]);
         }
-        while (stack.size() > 2) {
-            res += stack.pop() * stack.peek();
+        Set<Integer> min_cut = new HashSet<>();
+        min_cut.add(1);
+        int res = 0;
+        while (min_cut.size() < N)
+        {
+            int min_v = 0;
+            int min_w = Integer.MAX_VALUE;
+            for(int w : min_cut)
+            {
+                for(int adj : graph.get(w).keySet())
+                {
+                    if (!min_cut.contains(adj))
+                    {
+                        if (graph.get(w).get(adj) < min_w)
+                        {
+                            min_w = graph.get(w).get(adj);
+                            min_v = adj;
+                        }
+                    }
+                }
+            }
+            if (min_w == Integer.MAX_VALUE)
+                return -1;
+            min_cut.add(min_v);
+            res += min_w;
         }
         return res;
     }
     
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		ListNode node1 = new ListNode(1);
-		ListNode node2 = new ListNode(1);
-		ListNode node3 = new ListNode(3);
-		ListNode node4 = new ListNode(4);
-		ListNode node5 = new ListNode(0);
-		node1.next = node2;
-		node2.next = node3;
-		node3.next = node4;
-		node4.next = node5;
-		System.out.println(obj.insertionSortList(node1).val);
+		int[][] arr = {{}};
+		System.out.println(obj.minimumCost(5120, arr));
 	}
 }
