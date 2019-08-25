5a8ae1a5e9f1d32b0bad0febc957de45833bde86
Klimkina Liudmila <klimkina@gmail.com>
2019-08-24 21:52:20 -0700

    tailMap

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -55,7 +55,7 @@ class Solution {
         for (int i = 0; i < queries.length; i++)
         {
             Word w = new Word(queries[i]);
-            Iterator<Map.Entry<Integer, Integer>> it = freq.entrySet().iterator();
+            Iterator<Map.Entry<Integer, Integer>> it = freq.tailMap(w.freq, false).entrySet().iterator();
             while(it.hasNext())
             {
                 Map.Entry<Integer, Integer> entry = it.next();
