cd3671cd601d3bc492d40ecdc7b93df37716e3db
Klimkina Liudmila <klimkina@gmail.com>
2019-09-29 08:29:56 -0700

    remove dups

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -17,30 +17,54 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 class Solution {
-	public int equalSubstring(String s, String t, int maxCost) {
-        
+	public String removeDuplicates(String s, int k) {
         int n = s.length();
-        int[] costs = new int[n];
-        for (int i = 0; i < n; i++)
-            costs[i] = Math.abs(t.charAt(i) - s.charAt(i));
-        int len = 0;
-        int start = 0;
-        int cost = 0;
-        for (int i = 0; i < n; i++)
+        int[] pos = new int[n+1];
+        for (int i = 0; i <= n; i++)
+            pos[i] = i;
+        boolean removed = true;
+        while (removed)
         {
-            cost += costs[i];
-            while(cost > maxCost)
+            removed = false;
+            int start = pos[0];
+            int prev = 0;
+            while (start < n)
             {
-                cost -= costs[start++];
+                char ch = s.charAt(pos[start]);
+                int count = 1;
+                while (start < n && count < k)
+                {
+                    start = pos[start+1];
+                    if (s.charAt(start) != ch)
+                    {
+                        count = 1;
+                        ch = s.charAt(start);
+                        prev = start;
+                    }
+                    else
+                        count++;
+                }
+                if (count == k)
+                {
+                    pos[prev] = pos[start+1];
+                    removed = true;
+                    start = pos[start+1];
+                }
             }
-            len = Math.max(len, i-start+1);
         }
-        return len;
+        StringBuilder sb = new StringBuilder();
+        int start = pos[0];
+        while (start < n)
+        {
+            sb.append(s.charAt(start));
+            start = pos[start+1];
+        }
+        return sb.toString();
     }
 
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
 		
-		System.out.println(obj.equalSubstring("abcd", "acde", 0));
+		System.out.println(obj.removeDuplicates("pbbcggttciiippooaais", 2));
 	}
 }
