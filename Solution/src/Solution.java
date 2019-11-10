e9a5b0e2085a701406f45c5acdbdb0e17788b993
Klimkina Liudmila <klimkina@gmail.com>
2019-11-09 21:07:37 -0800

    Maximum Score Words Formed by Letters

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -16,57 +16,67 @@ import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-class Solution {
-	private class Sound implements Comparable<Sound>
-	{
-		Integer strength;
-		Integer dist;
-		public Sound(int s, int d)
-		{
-			strength = s;
-			dist = d;
-		}
-		@Override
-	    public int compareTo(Sound other){
-			return -this.strength + other.strength;
-		}
-	}
-	
-	public long minSound(List<Integer> strengths, List<Integer> threshold_dist) {
-	    // Write your code here
-		int res = 0;
-        PriorityQueue<Sound> pq = new PriorityQueue<>();
-        int n = strengths.size();
-        for (int i = 0; i < n; i++)
-        {
-            int s = strengths.get(i);
-            int t = threshold_dist.get(i);
-            
-            pq.offer(new Sound(s, t));
-        }
-        TreeSet<Integer> dists = new TreeSet<>();
-        for (int i = 0; i < strengths.size(); i++)
-            dists.add(i);
-        int start = 0;
-        while (!pq.isEmpty())
-        {
-            Sound s = pq.poll();
-            Integer low = dists.ceiling(s.dist);
-            if (low == null)
-            {
-                res += s.strength;
-                dists.pollFirst();
-            }
-            else
-                dists.remove(low);
-        }
-        
-        return res;
-	}
+/*
+Given a list of words, list of  single letters (might be repeating) and score of every character.
 
+Return the maximum score of any valid set of words formed by using the given letters (words[i] cannot be used two or more times).
+
+It is not necessary to use all characters in letters and each letter can only be used once. Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0], score[1], ... , score[25] respectively.
+ */
+class Solution {
+	public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int n = words.length;
+        int[] word_scores = new int[n];
+        int[][] word_letters = new int[n][26];
+        int[] let_count = new int[26];
+        for(int i = 0; i < letters.length; i++)
+            let_count[letters[i] - 'a']++; 
+        for(int j = 0; j < n; j++)
+            for(int i = 0; i < words[j].length(); i++)
+                word_letters[j][words[j].charAt(i) - 'a']++; 
+        for(int j = 0; j < n; j++)
+        {
+            for(int i = 0; i < 26; i++)
+            {
+                if (word_letters[j][i] > let_count[i])
+                {
+                    word_scores[j] = 0;
+                    break;
+                }
+                word_scores[j] += score[i]* word_letters[j][i];
+            }
+        }
+        return maxScoreWords(word_letters, let_count, word_scores, 0);
+    }
+    private int maxScoreWords(int[][] word_letters, int[] let_count, int[] word_scores, int start) {
+        if (start >= word_scores.length)
+            return 0;
+        if(word_scores[start] == 0 || getScore(let_count, word_letters[start]) == 0)
+            return maxScoreWords(word_letters, let_count, word_scores, start + 1);
+        
+        for(int i = 0; i < 26; i++)
+            let_count[i] -= word_letters[start][i];
+        int res2 = maxScoreWords(word_letters, let_count, word_scores, start + 1) + word_scores[start];
+        for(int i = 0; i < 26; i++)
+            let_count[i] += word_letters[start][i];
+        int res1 = maxScoreWords(word_letters, let_count, word_scores, start + 1);
+        return Math.max(res1, res2);
+    }
+    private int getScore(int[] let_count, int[] word_count)
+    {
+    	for(int i = 0; i < 26; i++)
+        {
+            if (word_count[i] > let_count[i])
+            	return 0;
+        }
+    	return 1;
+    }
+    
 	public static void main(String[] args) {   	
 		Solution obj = new Solution();
-		
-		System.out.println(obj.removeDuplicates("pbbcggttciiippooaais", 2));
+		String[] words = {"dog","cat","dad","good"};
+		char[] letters = {'a','a','c','d','d','d','g','o','o'};
+		int[] score = {1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
+		System.out.println(obj.maxScoreWords(words, letters, score));
 	}
 }
